296a656b1300b109562a09786c604756d3493dc8
Deleplanque Tristan <deleplanque@ipgp.fr>
2017-04-11 10:48:21 +0200

    initial commit structure du code - recuperation position station - recuperation position faille - calcul tps de trajet - ARF figs - stacks - plots

---
+++ kumamoto_configuration.py
@@ -0,0 +1,16 @@
+import numpy as np
+import math
+import cmath
+import matplotlib.pyplot
+
+#recuperation position stations
+
+#recuperation position faille
+
+#calcul matrice temps de trajet
+
+#ARF figures
+
+#stacks
+
+#plots
