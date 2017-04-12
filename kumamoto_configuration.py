018ef416cf618d0fa5d3ae4fbc002c04163d82af
Tristan Deleplanque <deleplanque@snoopy.ipgp.fr>
2017-04-12 14:13:50 +0200

    plot carte avec position des stations

--- kumamoto_configuration.py
+++ kumamoto_configuration.py
@@ -1,8 +1,9 @@
-import numpy
+import numpy as np
 import math
 import cmath
-import matplotlib.pyplot
+import matplotlib.pyplot as plt
 import os
+from mpl_toolkits.basemap import Basemap
 
 #recuperation position stations
 path = '/home/deleplanque/Documents/back_proj/en_cours'
@@ -46,22 +47,22 @@ for fichier in list_fichier1:
     info = info.split('\n')
     info_stations.append((info[0].split(' ')[8],
                           info[0].split(' ')[9],
-                          info[1].split(' ')[14],
-                          info[2].split(' ')[13],
-		          info[3].split(' ')[8],
-		          info[4].split(' ')[14],
+                          float(info[1].split(' ')[14]),
+                          float(info[2].split(' ')[13]),
+		          float(info[3].split(' ')[8]),
+		          float(info[4].split(' ')[14]),
 		          'KiK-net',
 		          info[5].split(' ')[7],
-		          info[6].split(' ')[7],
-		          info[7].split(' ')[6],
-		          info[8].split(' ')[2],
+		          float(info[6].split(' ')[7]),
+		          float(info[7].split(' ')[6]),
+		          float(info[8].split(' ')[2]),
                           info[9].split(' ')[8],
                           info[9].split(' ')[9],
                           info[10].split(' ')[2],
-                          info[11].split(' ')[3],
+                          float(info[11].split(' ')[3]),
                           info[12].split(' ')[14],
                           info[13].split(' ')[6],
-                          info[14].split(' ')[5],
+                          float(info[14].split(' ')[5]),
                           info[15].split(' ')[4],
                           info[15].split(' ')[5]
 			 ))
@@ -74,42 +75,71 @@ for fichier in list_fichier2:
     info = info.split('\n')
     info_stations.append((info[0].split(' ')[8],
                           info[0].split(' ')[9],
-                          info[1].split(' ')[14],
-                          info[2].split(' ')[13],
-                          info[3].split(' ')[8],
-                          info[4].split(' ')[14],
+                          float(info[1].split(' ')[14]),
+                          float(info[2].split(' ')[13]),
+                          float(info[3].split(' ')[8]),
+                          float(info[4].split(' ')[14]),
                           'K-NET',
                           info[5].split(' ')[7],
-                          info[6].split(' ')[7],
-                          info[7].split(' ')[6],
-                          info[8].split(' ')[2],
+                          float(info[6].split(' ')[7]),
+                          float(info[7].split(' ')[6]),
+                          float(info[8].split(' ')[2]),
                           info[9].split(' ')[8],
                           info[9].split(' ')[9],
                           info[10].split(' ')[2],
-                          info[11].split(' ')[3],
+                          float(info[11].split(' ')[3]),
                           info[12].split(' ')[14],
                           info[13].split(' ')[6],
-                          info[14].split(' ')[5],
+                          float(info[14].split(' ')[5]),
                           info[15].split(' ')[4],
                           info[15].split(' ')[5]
                          ))
     data.close()
 
-print info_stations
-
 #print position des stations
 
 lat_sta = [a[8] for a in info_stations]
 long_sta = [a[9] for a in info_stations]
+color_sta = ['b' if a[6] == 'KiK-net' else 'r' for a in info_stations]
+code_sta = [a[7] for a in info_stations]
 del lat_sta[0]
 del long_sta[0]
+del color_sta[0]
+del code_sta[0]
 
 os.chdir(path_results)
 
-fig_pos_sta, ax_pos_sta = matplotlib.pyplot.subplots(1, 1)
-ax_pos_sta.set_xlabel('Long.')
-ax_pos_sta.set_ylabel('Lat.')
-ax_pos_sta.plot(long_sta, lat_sta, marker='o', linestyle='none')
+fig_pos_sta, ax_pos_sta = plt.subplots(1, 1)
+#ax_pos_sta.set_xlabel('Long.')
+#ax_pos_sta.set_ylabel('Lat.')
+m = Basemap(projection='merc',
+	    llcrnrlon=128,
+	    llcrnrlat=30,
+	    urcrnrlon=140,
+	    urcrnrlat=37,
+	    resolution='h'
+	   )
+x, y = m(long_sta, lat_sta)
+m.drawcoastlines(linewidth=0.2)
+m.fillcontinents('yellow')
+m.drawparallels(np.arange(30, 38, 2), labels=[1, 0, 0, 0], linewidth=0)
+m.drawmeridians(np.arange(128, 141, 2), labels=[0, 0, 0, 1], linewidth=0)
+ax_pos_sta.scatter(x,
+		   y,
+		   3,
+		   marker='^',
+		   color=color_sta,
+		   zorder=2
+		  )
+for i in range(len(code_sta)):
+    ax_pos_sta.text(x[i],
+		    y[i],
+		    code_sta[i],
+		    fontsize=3,
+		    ha='center',
+		    va='bottom',
+		    zorder=3
+		   )
 fig_pos_sta.savefig('map_stations.pdf')
 
 #recuperation position faille
