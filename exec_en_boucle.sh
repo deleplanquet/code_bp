a28f695907ecd0de55866e10374eb0109c8ba6a0
Deleplanque Tristan <deleplanque@ipgp.fr>
2017-05-04 15:14:36 +0200

    details

--- exec_en_boucle.sh
+++ exec_en_boucle.sh
@@ -10,9 +10,7 @@ cd Documents/back_proj/en_cours/code/
 
 for dossier in $liste_dossiers; do
 	echo $dossier
-	echo ../../../../../../media/deleplanque/Lexar/Results/Kumamoto/$dossier
 	if [ ! -e ../../../../../../media/deleplanque/Lexar/Results/Kumamoto/$dossier ]; then
-		echo prout
 		python3 kumamoto_configuration.py $dossier
 	fi
 done
