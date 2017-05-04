94fa556e7d04b028fc61cd0468a699a93996bc81
Deleplanque Tristan <deleplanque@ipgp.fr>
2017-05-04 17:02:29 +0200

    methode globale de conversion en format 'SAC'

---
+++ boucle_tosac.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+cd /localstorage/deleplanque/Data/Kumamoto/
+
+liste_dossiers=`ls -d */`
+
+for dossier in $liste_dossiers; do
+	cd /localstorage/deleplanque/Data/Kumamoto/
+	dodo=${dossier:0:14}
+	echo $dodo
+	if [ ! -e /localstorage/deleplanque/Data/Kumamoto_sac/$dodo ]; then
+		cd $dodo/
+		../../../Codes/tosac.py $dodo.kik
+		../../../Codes/tosac.py $dodo.knt
+	fi
+done
--- tosac.py
+++ tosac.py
@@ -1,7 +1,29 @@
 #!/usr/bin/env python
 import sys
 from obspy import read
+import os
 
-for infile in sys.argv[1:]:
-   tr = read(infile)[0]
-   tr.write(infile + '.sac', format='SAC')
+path_data = '/localstorage/deleplanque/Data/Kumamoto'
+path_sac = '/localstorage/deleplanque/Data/Kumamoto_sac'
+os.chdir(path_sac)
+
+dossier = sys.argv[1]
+ext = dossier[-4:]
+dossier = dossier[0:-4]
+
+if os.path.isdir(str(dossier)) == False:
+    os.makedirs(str(dossier))
+
+path_dossier = path_data + '/' + str(dossier) + '/' + str(dossier) + str(ext)
+
+list_fichiers = os.listdir(path_dossier)
+if ext == '.kik':
+    list_fichiers = [a for a in list_fichiers if '.NS2' in a]
+else:
+    list_fichiers = [a for a in list_fichiers if '.UD' in a]
+
+for fichier in list_fichiers:
+    os.chdir(path_dossier)
+    tr = read(fichier)[0]
+    os.chdir(path_sac + '/' + str(dossier))
+    tr.write(fichier + '.sac', format='SAC')
