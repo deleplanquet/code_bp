5805f3617b651e5e342a886250858e86e5fba2b6
Deleplanque Tristan <deleplanque@ipgp.fr>
2017-05-09 14:47:44 +0200

    toutes les traces sur le meme graphe + un graphe par seisme

---
+++ traces_fct_distance.py
@@ -0,0 +1,76 @@
+from obspy import read
+import math
+import os
+import matplotlib.pyplot as plt
+import numpy as np
+from scipy.signal import hilbert
+from obspy.signal.util import smooth
+
+#constantes
+R_Earth = 6400
+
+#conversion angle degre -> radian
+def d2r(angle):
+    return angle*math.pi/180
+
+#conversion coordonnees geographiques -> cartesien
+def geo2cart(r, lat, lon):
+    rlat = d2r(lat)
+    rlon = d2r(lon)
+    xx = r*math.cos(rlat)*math.cos(rlon)
+    yy = r*math.cos(rlat)*math.sin(rlon)
+    zz = r*math.sin(rlat)
+    return [xx, yy, zz]
+
+#distance entre deux points, coordonnees cartesiennes
+def dist(la1, lo1, el1, la2, lo2, el2):
+    x1, y1, z1 = geo2cart(R_Earth + el1, la1, lo1)
+    x2, y2, z2 = geo2cart(R_Earth + el2, la2, lo2)
+    return pow(pow(x1 - x2, 2) + pow(y1 - y2, 2) + pow(z1 - z2, 2), 0.5)
+
+list_dossier = ['20160417054100', '20160415015900', '20160416074900', '20160416131700', '20160415124600', '20160416220600']
+
+path = '/localstorage/deleplanque'
+path_data = path + '/Data/Kumamoto_sac'
+path_results = path + '/Results'
+
+fig, ax = plt.subplots(1, 1)
+ax.set_xlabel('Time (s)')
+
+for dossier in list_dossier:
+    print(dossier)
+    os.chdir(path_data + '/' + dossier)
+    list_fichier = os.listdir(path_data + '/' + dossier)
+    list_fichier = [a for a in list_fichier if (('UD' in a) == True and ('UD1' in a) == False)]
+
+    fig2, ax2 = plt.subplots(1, 1)
+    ax2.set_xlabel('Time (s)')
+
+    for fichier in list_fichier:
+    	st = read(fichier)
+    	tstart = st[0].stats.starttime + st[0].stats.sac.t0 - 15
+    	tend = tstart + 50
+    	st[0].trim(tstart, tend)
+    	st = st.detrend(type = 'constant')
+    	st.normalize()
+    	tr_brut = st[0]
+    	tr_filt = tr_brut.filter('bandpass', freqmin=0.2, freqmax=10, corners=4, zerophase=True)
+    	envelop = abs(hilbert(tr_filt))
+    	env_smoothed = smooth(envelop, 20)
+
+    	t = np.arange(tr_brut.stats.npts)/tr_brut.stats.sampling_rate
+
+    	ordo = dist(st[0].stats.sac.stla, st[0].stats.sac.stlo, 0.001*st[0].stats.sac.stel, st[0].stats.sac.evla, st[0].stats.sac.evlo, -st[0].stats.sac.evdp)
+
+    	ax.plot(t, 5*env_smoothed + ordo, linewidth = 0.2)
+    	ax.text(40, ordo, st[0].stats.station)
+
+    	ax2.plot(t, 5*env_smoothed + ordo, linewidth = 0.2)
+    	ax2.text(40, ordo, st[0].stats.station)
+
+    os.chdir(path_results)
+    ax2.axvline(15)
+    fig2.savefig('tt_sta_' + dossier + '.pdf')
+
+ax.axvline(15)
+fig.savefig('tt_sta_tt_seisme.pdf')
