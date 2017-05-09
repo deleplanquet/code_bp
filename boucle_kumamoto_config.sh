343698eea24de5e1385a2f7214c1427735babf4e
Deleplanque Tristan <deleplanque@ipgp.fr>
2017-05-09 15:31:51 +0200

    details

--- boucle_kumamoto_config.sh
+++ boucle_kumamoto_config.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
-#cd /localstorage/deleplanque/Data/Kumamoto_sac/
-cd /Users/deleplanque/Documents/Data/Kumamoto_sac/
+cd /localstorage/deleplanque/Data/Kumamoto_sac/
+#cd /Users/deleplanque/Documents/Data/Kumamoto_sac/
 
 liste_dossiers=`ls -d */`
 
-#cd /localstorage/deleplanque/Codes/
-cd /Users/deleplanque/Documents/Codes/
+cd /localstorage/deleplanque/Codes/
+#cd /Users/deleplanque/Documents/Codes/
 
 for dossier in $liste_dossiers; do
 	dodo=${dossier:0:14}
 	echo $dodo
-#	if [ ! -e /localstorage/deleplanque/Results/Kumamoto/$dossier/ ]; then
-	if [ ! -e /Users/deleplanque/Documents/Results/Kumamoto/$dossier/ ]; then
+	if [ ! -e /localstorage/deleplanque/Results/Kumamoto/$dossier/ ]; then
+#	if [ ! -e /Users/deleplanque/Documents/Results/Kumamoto/$dossier/ ]; then
 		python3 kumamoto_configuration.py $dossier
 	fi
 done
