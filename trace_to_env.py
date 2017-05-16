61e97c42dc25c9f8cf68fbab3ed5e66476afe8b9
Deleplanque Tristan <deleplanque@ipgp.fr>
2017-05-16 08:38:57 +0200

    trim a 5 seconde avant le pointage P et change sac.a et sac.t0 qui ne s adapte pas au trim automatiquement

--- trace_to_env.py
+++ trace_to_env.py
@@ -21,13 +21,20 @@ if os.path.isdir(str(dossier)) == False:
     os.makedirs(str(dossier))
 
 list_fich = os.listdir(path_data)
+list_fich = [a for a in list_fich if ('UD' in a) == True and ('UD1' in a) == False]
 
 for station in list_fich:
     print(station)
     os.chdir(path_data)
     st = read(station)
     st.detrend(type = 'constant')
-    tr = st[0].filter('bandpass', freqmin = 0.2, freqmax = 10, corners = 4, zerophase = True)
+    old_a = st[0].stats.sac.a
+    tstart = st[0].stats.starttime + st[0].stats.sac.a - 5
+    st[0].stats.sac.a = 5
+    st[0].stats.sac.t0 = st[0].stats.sac.t0 - old_a + 5
+    tend = tstart + 50
+    tr = st[0].trim(tstart, tend, pad=True, fill_value=0)
+    tr = tr.filter('bandpass', freqmin = 0.2, freqmax = 10, corners = 4, zerophase = True)
     tr = [a**2 for a in tr]
     tr = np.asarray(smooth(tr, 20))
     tr = Trace(tr, st[0].stats)
