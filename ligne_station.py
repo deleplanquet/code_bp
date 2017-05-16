e3137959d5610748f7684d28a0def20489fa479f
Deleplanque Tristan <deleplanque@ipgp.fr>
2017-05-16 17:25:10 +0200

    utilisation des envelopes changement de ligne pour avoir plus de stations

--- ligne_station.py
+++ ligne_station.py
@@ -29,13 +29,10 @@ def dist(la1, lo1, el1, la2, lo2, el2):
     return pow(pow(x1 - x2, 2) + pow(y1 - y2, 2) + pow(z1 - z2, 2), 0.5)
 
 #normalisation
-def norm(vect):
-    norm_v = 0
-    for a in vect:
-    	norm_v = norm_v + a*a
-    return [30*a/pow(norm_v, 0.5) for a in vect]
+def norm1(vect):
+    return [10*a/vect.max() for a in vect]
 
-name_dossier = '20160415015900'
+name_dossier = '20160416080200'
 #name_dossier = '20160417054100'
 #name_dossier = '20160416074900'
 #name_dossier = '20160416131700'
@@ -44,12 +41,13 @@ name_dossier = '20160415015900'
 
 #path = '/localstorage/deleplanque'
 path = '/Users/deleplanque/Documents'
-path_data = path + '/Data/Kumamoto_sac/' + name_dossier
+path_data = path + '/Data/Kumamoto_env/' + name_dossier
 path_results = path + '/Results'
 
 os.chdir(path_data)
 
-list_station = ['KMMH161604150159.UD2.sac', 'KMMH031604150159.UD2.sac', 'FKOH101604150159.UD2.sac']
+#list_station = ['env_KMMH141604160802.UD2.sac', 'env_KMM0111604160802.UD.sac', 'env_MYZ0201604160802.UD.sac', 'env_MYZH051604160802.UD2.sac', 'env_MYZH081604160802.UD2.sac']
+list_station = ['env_KMMH141604160802.UD2.sac', 'env_KMM0141604160802.UD.sac', 'env_KMMH131604160802.UD2.sac', 'env_KMM0171604160802.UD.sac', 'env_MYZ0101604160802.UD.sac', 'env_MYZH131604160802.UD2.sac']
 #list_station = ['KMM0111604170541.UD.sac', 'MYZ0201604170541.UD.sac', 'MYZH081604170541.UD2.sac']
 #list_station = ['KMMH031604160749.UD2.sac', 'KMM0021604160749.UD.sac', 'FKO0161604160749.UD.sac']
 #list_station = ['KMMH161604161317.UD2.sac', 'KMM0091604161317.UD.sac', 'MYZ0011604161317.UD.sac']
@@ -62,26 +60,25 @@ ax.set_ylabel('Distance from the hypocenter (km)')
 
 for station in list_station:
     st = read(station)
-    st = st.detrend(type='constant')
+    #st = st.detrend(type='constant')
     tstart = st[0].stats.starttime + st[0].stats.sac.t0 - 15
-    print(st[0].stats.sac.b)
     tend = tstart + 50
     st[0].trim(tstart, tend, pad=True, fill_value=0)
-    tr_brut = st[0]
-    tr_filt = tr_brut.filter('bandpass', freqmin=0.2, freqmax=10, corners=4, zerophase=True)
-    envelop = abs(hilbert(tr_filt))
-    env_smoothed2 = smooth(envelop, 20)
-    squared_tr = [a**2 for a in tr_filt]
-    env_smoothed = smooth(squared_tr, 20)
+    #tr_brut = st[0]
+    #tr_filt = tr_brut.filter('bandpass', freqmin=0.2, freqmax=10, corners=4, zerophase=True)
+    #envelop = abs(hilbert(tr_filt))
+    #env_smoothed2 = smooth(envelop, 20)
+    #squared_tr = [a**2 for a in tr_filt]
+    #env_smoothed = smooth(squared_tr, 20)
 
-    t = np.arange(tr_brut.stats.npts)/tr_brut.stats.sampling_rate
+    t = np.arange(st[0].stats.npts)/st[0].stats.sampling_rate
 
     ordo = dist(st[0].stats.sac.stla, st[0].stats.sac.stlo, 0.001*st[0].stats.sac.stel, st[0].stats.sac.evla, st[0].stats.sac.evlo, -st[0].stats.sac.evdp)
 
     #ax.plot(t, tr_brut, color='black')
-    ax.plot(t, norm(env_smoothed) + ordo, linewidth=0.2, color='blue')
-    ax.plot(t, norm(env_smoothed2) + ordo, linewidth=0.2, color='red')
-    ax.axvline(15)
+    ax.plot(t, norm1(st[0].data) + ordo, linewidth=0.2, color='black')
+    ax.axvline(15, linewidth = 0.2, color = 'darkorange')
+    ax.scatter(15 - st[0].stats.sac.t0 + st[0].stats.sac.a, ordo, 2, color = 'steelblue')
 
     #ax.text(0, 0, name_dossier)
     ax.text(40, ordo, st[0].stats.station)
