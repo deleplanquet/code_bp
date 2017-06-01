3aeeab7e5f52942c5d8c18c0f51231a7c86e3061
Deleplanque Tristan <deleplanque@ipgp.fr>
2017-06-01 22:35:46 +0900

    ajout des deux solutions de strike dip rake

--- ref_seismes.py
+++ ref_seismes.py
@@ -19,6 +19,12 @@ with open('ref_seismes.txt', 'r') as mon_fich:
         a['lat'] = my_list[4]
         a['lon'] = my_list[5]
         a['dep'] = my_list[6]
+        a['str1'] = my_list[7]
+        a['dip1'] = my_list[8]
+        a['rak1'] = my_list[9]
+        a['str2'] = my_list[10]
+        a['dip2'] = my_list[11]
+        a['rak2'] = my_list[12]
         A[my_list[0]] = a
 
 with open('ref_seismes_bin', 'wb') as ma_sortie:
