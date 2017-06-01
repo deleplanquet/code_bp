aac5a0121e483e68a05229051cf83140c8377283
Deleplanque Tristan <deleplanque@ipgp.fr>
2017-06-01 12:59:27 +0900

    fait a la fois les stations kiknet et knet correction typo du path

--- tosac.py
+++ tosac.py
@@ -4,26 +4,34 @@ from obspy import read
 import os
 import obspy.io.sac
 
-paht = os.getcwd()[:-6]
-path_data = path + '/Data/Kumamoto'
-path_sac = path + '/Data/Kumamoto_sac'
-os.chdir(path_sac)
-
 dossier = sys.argv[1]
-ext = dossier[-4:]
-dossier = dossier[0:-4]
 
-if os.path.isdir(str(dossier)) == False:
-    os.makedirs(str(dossier))
+path = os.getcwd()[:-6]
+path_data = path + '/Data/Kumamoto/' + dossier
+path_kik = path_data + '/' + dossier + '.kik'
+path_knt = path_data + '/' + dossier + '.knt'
+path_sac = path + '/Data/Kumamoto_sac/' + dossier
 
-path_dossier = path_data + '/' + str(dossier) + '/' + str(dossier) + str(ext)
+if os.path.isdir(path_sac) == False:
+    os.makedirs(path_sac)
 
-list_fichiers = os.listdir(path_dossier)
+list_fichiers = os.listdir(path_kik)
 list_fichiers = [a for a in list_fichiers if ('ps.gz' in a) == False]
 
 for fichier in list_fichiers:
-    os.chdir(path_dossier)
+    os.chdir(path_kik)
     tr = read(fichier)[0]
     tr.stats.sac = tr.stats.knet
-    os.chdir(path_sac + '/' + str(dossier))
+    os.chdir(path_sac)
     tr.write(fichier + '.sac', format='SAC')
+
+list_fichiers = os.listdir(path_knt)
+list_fichiers = [a for a in list_fichiers if ('ps.gz' in a) == False]
+
+for fichier in list_fichiers:
+    os.chdir(path_knt)
+    tr = read(fichier)[0]
+    tr.stats.sac = tr.stats.knet
+    os.chdir(path_sac)
+    tr.write(fichier + '.sac', format='SAC')
+
