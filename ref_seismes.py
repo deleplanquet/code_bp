a4470475646b42d24b75065ed2176b17441f8006
Deleplanque Tristan <deleplanque@ipgp.fr>
2017-06-05 11:56:41 +0900

    dans le fichier des seismes.txt les titres de chaque colonne ne sont pas pris en compte dans la creation du dictionnaire

--- ref_seismes.py
+++ ref_seismes.py
@@ -13,19 +13,20 @@ with open('ref_seismes.txt', 'r') as mon_fich:
     for line in mon_fich:
         a = {}
         my_list = line.split()
-        a['nFnet'] = my_list[1]
-        a['Mw'] = float(my_list[2])
-        a['Mj'] = float(my_list[3])
-        a['lat'] = float(my_list[4])
-        a['lon'] = float(my_list[5])
-        a['dep'] = float(my_list[6])
-        a['str1'] = float(my_list[7])
-        a['dip1'] = float(my_list[8])
-        a['rak1'] = float(my_list[9])
-        a['str2'] = float(my_list[10])
-        a['dip2'] = float(my_list[11])
-        a['rak2'] = float(my_list[12])
-        A[my_list[0]] = a
+        if (my_list[0] == 'Name') == False:
+            a['nFnet'] = my_list[1]
+            a['Mw'] = float(my_list[2])
+            a['Mj'] = float(my_list[3])
+            a['lat'] = float(my_list[4])
+            a['lon'] = float(my_list[5])
+            a['dep'] = float(my_list[6])
+            a['str1'] = float(my_list[7])
+            a['dip1'] = float(my_list[8])
+            a['rak1'] = float(my_list[9])
+            a['str2'] = float(my_list[10])
+            a['dip2'] = float(my_list[11])
+            a['rak2'] = float(my_list[12])
+            A[my_list[0]] = a
 
 with open('ref_seismes_bin', 'wb') as ma_sortie:
     mon_pick = pickle.Pickler(ma_sortie)
