3c339e32fcc8a1d546e65b7632b9223d774cebbe
Deleplanque Tristan <deleplanque@ipgp.fr>
2017-06-08 22:30:17 +0900

    va chercher les vitesses au lieu des accelerations`

--- trace_to_env.py
+++ trace_to_env.py
@@ -10,14 +10,11 @@ import matplotlib.pyplot as plt
 dossier = sys.argv[1]
 
 path = os.getcwd()[:-6]
-path_data = path + '/Data/Kumamoto_sac_inf100km/' + str(dossier)
-path_env = path + '/Data/Kumamoto_env/'
-path_env_dossier = path_env + str(dossier)
+path_data = path + '/Data/Kumamoto_vel/' + str(dossier)
+path_env = path + '/Data/Kumamoto_env/' + str(dossier)
 
-os.chdir(path_env)
-
-if os.path.isdir(str(dossier)) == False:
-    os.makedirs(str(dossier))
+if os.path.isdir(path_env) == False:
+    os.makedirs(path_env)
 
 list_fich = os.listdir(path_data)
 list_fich = [a for a in list_fich if ('UD' in a) == True and ('UD1' in a) == False]
@@ -26,18 +23,11 @@ for station in list_fich:
     print(station)
     os.chdir(path_data)
     st = read(station)
-    st.detrend(type = 'constant')
-    old_a = st[0].stats.sac.a
-    tstart = st[0].stats.starttime + st[0].stats.sac.a - 5
-    st[0].stats.sac.a = 5
-    st[0].stats.sac.t0 = st[0].stats.sac.t0 - old_a + 5
-    tend = tstart + 50
-    tr = st[0].trim(tstart, tend, pad=True, fill_value=0)
-    tr = tr.filter('bandpass', freqmin = 0.2, freqmax = 10, corners = 4, zerophase = True)
+    tr = st[0].filter('bandpass', freqmin = 0.2, freqmax = 10, corners = 4, zerophase = True)
     tr = [a**2 for a in tr]
     tr = np.asarray(smooth(tr, 20))
     tr = Trace(tr, st[0].stats)
-    os.chdir(path_env_dossier)
+    os.chdir(path_env)
     tr.write('env_' + station, format = 'SAC')
 
 
