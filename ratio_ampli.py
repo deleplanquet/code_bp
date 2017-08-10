5b9a8d1f8b6a6f5338c0324ce75efef124075f5a
Deleplanque Tristan <deleplanque@ipgp.fr>
2017-08-10 17:56:26 +0200

    permet de calculer l'amplitude associer a chaque fenetre de temps pour chaque station et chaque frequence

---
+++ ratio_ampli.py
@@ -0,0 +1,58 @@
+import pickle
+from obspy import read
+import os
+import sys
+
+dossier = sys.argv[1]
+
+path_origin = os.getcwd()[:-6]
+path = path_origin + '/Kumamoto/' + dossier
+path_data1 = path + '/' + dossier + '_vel_env_02_05Hz'
+path_data2 = path + '/' + dossier + '_vel_env_05_1Hz'
+path_data3 = path + '/' + dossier + '_vel_env_1_2Hz'
+path_data4 = path + '/' + dossier + '_vel_env_2_4Hz'
+path_data5 = path + '/' + dossier + '_vel_env_4_10Hz'
+
+list_fich = os.listdir(path_data1)
+
+t_wd = 1
+ampli = {}
+lst_frq = ['02_05', '05_1', '1_2', '2_4', '4_10']
+
+for freq in range(5):
+    frq = {}
+    for station in list_fich:
+	lst = []
+	os.chdir(path_data1)
+	st = read(station)
+	for i in range(int(50./t_wd)):
+	    for j in range(int(st[0].stats.sampling_rate*t_wd)):
+		clcl = clcl + st.data[int(i*t_wd*st[0].stats.sampling_rate) + j]/st[0].stats.sampling_rate
+	    lst.append(clcl)
+	frq[st.stats.station] = lst
+    ampli{lst_frq[freq]} = frq
+
+with open('mat_vel_ampli_bin', 'wb') as ma_sortie:
+    mon_pick = pickle.Pickler(ma_sortie)
+    mon_pick.dump(ampli)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
