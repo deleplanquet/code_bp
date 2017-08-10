47240a13971580bfdb1b602b44a2322b2d11c57f
Deleplanque Tristan <deleplanque@ipgp.fr>
2017-08-10 12:00:22 +0200

    fonctionne

--- 3components.py
+++ 3components.py
@@ -3,6 +3,7 @@ from obspy import Trace
 import os
 import sys
 import math
+import numpy as np
 
 dossier = sys.argv[1]
 
@@ -18,16 +19,22 @@ list_fich_x = [a for a in list_fich if ('EW' in a) == True and ('EW1' in a) == F
 list_fich_y = [a for a in list_fich if ('NS' in a) == True and ('NS1' in a) == False]
 list_fich_z = [a for a in list_fich if ('UD' in a) == True and ('UD1' in a) == False]
 
+list_fich_x.sort()
+list_fich_y.sort()
+list_fich_z.sort()
+
 for station in list_fich_x:
 	os.chdir(path_data)
 	stx = read(station)
-	sty = read(list_fich_y(list_fich_x.index(station)))
-	stz = read(list_fich_z(list_fich_x.index(station)))
+	sty = read(list_fich_y[list_fich_x.index(station)])
+	stz = read(list_fich_z[list_fich_x.index(station)])
 	if stx[0].stats.station == sty[0].stats.station and stx[0].stats.station == stz[0].stats.station:
 		tr_x = stx[0]
 		tr_y = sty[0]
 		tr_z = stz[0]
-		tr = [(c/abs(c))*sqrt(a**2 + b**2 + c**2) for a,b,c in zip(tr_x, tr_y, tr_z)]
+		tr_s = [a/abs(a) if abs(a)>0 else 0 for a in tr_z]
+		tr = [d*math.sqrt(a**2 + b**2 + c**2) for a,b,c,d in zip(tr_x, tr_y, tr_z, tr_s)]
+		tr = np.asarray(tr)
 		os.chdir(path_results)
 		tr = Trace(tr, stz[0].stats)
 		tr.write(station, format = 'SAC')
