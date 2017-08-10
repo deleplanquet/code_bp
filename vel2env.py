d99e378e752a01c7df715c0e2804c6e3f8fac957
Deleplanque Tristan <deleplanque@ipgp.fr>
2017-08-10 15:45:16 +0200

    correction logique

--- vel2env.py
+++ vel2env.py
@@ -36,35 +36,48 @@ for station in list_fich:
     os.chdir(path_data)
     st = read(station)
     tr02 = st[0].filter('bandpass', freqmin = 0.2, freqmax = 0.5, corners = 4, zerophase = True)
-    tr05 = st[0].filter('bandpass', freqmin = 0.5, freqmax = 1, corners = 4, zerophase = True)
-    tr1 = st[0].filter('bandpass', freqmin = 1, freqmax = 2, corners = 4, zerophase = True)
-    tr2 = st[0].filter('bandpass', freqmin = 2, freqmax = 4, corners = 4, zerophase = True)
-    tr4 = st[0].filter('bandpass', freqmin = 4, freqmax = 10, corners = 4, zerophase = True)
     tr02 = [a**2 for a in tr02]
-    tr05 = [a**2 for a in tr05]
-    tr1 = [a**2 for a in tr1]
-    tr2 = [a**2 for a in tr2]
-    tr4 = [a**2 for a in tr4]
     tr02 = np.asarray(smooth(tr02, 20))
-    tr05 = np.asarray(smooth(tr05, 20))
-    tr1 = np.asarray(smooth(tr1, 20))
-    tr2 = np.asarray(smooth(tr2, 20))
-    tr4 = np.asarray(smooth(tr4, 20))
     tr02 = Trace(tr02, st[0].stats)
-    tr05 = Trace(tr05, st[0].stats)
-    tr1 = Trace(tr1, st[0].stats)
-    tr2 = Trace(tr2, st[0].stats)
-    tr4 = Trace(tr4, st[0].stats)
     os.chdir(path_env_02)
     tr02.write('02_05Hz_' + station, format = 'SAC')
+
+    os.chdir(path_data)
+    st = read(station)
+    tr05 = st[0].filter('bandpass', freqmin = 0.5, freqmax = 1, corners = 4, zerophase = True)
+    tr05 = [a**2 for a in tr05]
+    tr05 = np.asarray(smooth(tr05, 20))
+    tr05 = Trace(tr05, st[0].stats)
     os.chdir(path_env_05)
     tr05.write('05_1Hz_' + station, format = 'SAC')
+
+    os.chdir(path_data)
+    st = read(station)
+    tr1 = st[0].filter('bandpass', freqmin = 1, freqmax = 2, corners = 4, zerophase = True)
+    tr1 = [a**2 for a in tr1]
+    tr1 = np.asarray(smooth(tr1, 20))
+    tr1 = Trace(tr1, st[0].stats)
     os.chdir(path_env_1)
     tr1.write('1_2Hz_' + station, format = 'SAC')
+
+    os.chdir(path_data)
+    st = read(station)
+    tr2 = st[0].filter('bandpass', freqmin = 2, freqmax = 4, corners = 4, zerophase = True)
+    tr2 = [a**2 for a in tr2]
+    tr2 = np.asarray(smooth(tr2, 20))
+    tr2 = Trace(tr2, st[0].stats)
     os.chdir(path_env_2)
     tr2.write('2_4Hz_' + station, format = 'SAC')
+
+    os.chdir(path_data)
+    st = read(station)
+    tr4 = st[0].filter('bandpass', freqmin = 4, freqmax = 10, corners = 4, zerophase = True)
+    tr4 = [a**2 for a in tr4]
+    tr4 = np.asarray(smooth(tr4, 20))
+    tr4 = Trace(tr4, st[0].stats)
     os.chdir(path_env_4)
     tr4.write('4_10Hz_' + station, format = 'SAC')
+
 
 
 
