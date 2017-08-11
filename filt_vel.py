4c4d844e3cc6cfc0339e57469368ca150320130c
Deleplanque Tristan <deleplanque@ipgp.fr>
2017-08-11 19:12:36 +0200

    correction nom fichier sortie

--- filt_vel.py
+++ filt_vel.py
@@ -8,52 +8,49 @@ dossier = sys.argv[1]
 path_origin = os.getcwd()[:-6]
 path = path_origin + '/Kumamoto/' + dossier
 path_data = path + '/' + dossier + '_vel'
-path_rslt1 = path + '/' + dossier + '_vel_02_05Hz'
-path_rslt2 = path + '/' + dossier + '_vel_05_1Hz'
-path_rslt3 = path + '/' + dossier + '_vel_1_2Hz'
-path_rslt4 = path + '/' + dossier + '_vel_2_4Hz'
-path_rslt5 = path + '/' + dossier + '_vel_4_10Hz'
 
-lst_pth_rslt = [path_rslt1, path_rslt2, path_rslt3, path_rslt4, path_rslt5]
+lst_frq = ['02_05', '05_1', '1_2', '2_4', '4_10']
+lst_pth_rslt = []
 
-for pth in lst_path_rslt:
-    if os.path.isdir(pth) == False:
-    	os.makedirs(pth)
+for freq in lst_frq:
+    lst_pth_rslt[lst_frq.index(freq)] = path + '/' + dossier + '_vel_' + freq + 'Hz'
+    if os.path.isdir(lst_pth_rslt[lst_frq.index(freq)]) == False:
+    	os.makedirs(lst_pth_rslt[lst_frq.index(freq)])
 
 lst_fch = os.listdir(path_data)
 
 for station in lst_fch:
-    print(station)
+    print('     ', station)
 
     os.chdir(path_data)
     st = read(station)
     tr = Trace(st[0].filter('bandpass', freqmin = 0.2, freqmax = 0.5, corners = 4, zerophase = True), st[0].stats)
     os.chdir(path_rslt1)
-    tr.write(station + '_02_05Hz', format = 'SAC')
+    tr.write(station[:-4] + '_02_05Hz.sac', format = 'SAC')
     
     os.chdir(path_data)
     st = read(station)
     tr = Trace(st[0].filter('bandpass', freqmin = 0.5, freqmax = 1, corners = 4, zerophase = True), st[0].stats)
     os.chdir(path_rslt2)
-    tr.write(station + '_05_1Hz', format = 'SAC')
+    tr.write(station[:-4] + '_05_1Hz.sac', format = 'SAC')
 
     os.chdir(path_data)
     st = read(station)
     tr = Trace(st[0].filter('bandpass', freqmin = 1, freqmax = 2, corners = 4, zerophase = True), st[0].stats)
     os.chdir(path_rslt3)
-    tr.write(station + '_1_2Hz', format = 'SAC')
+    tr.write(station[:-4] + '_1_2Hz.sac', format = 'SAC')
 
     os.chdir(path_data)
     st = read(station)
     tr = Trace(st[0].filter('bandpass', freqmin = 2, freqmax = 4, corners = 4, zerophase = True), st[0].stats)
     os.chdir(path_rslt4)
-    tr.write(station + '_2_4Hz', format = 'SAC')
+    tr.write(station[:-4] + '_2_4Hz.sac', format = 'SAC')
 
     os.chdir(path_data)
     st = read(station)
     tr = Trace(st[0].filter('bandpass', freqmin = 4, freqmax = 10, corners = 4, zerophase = True), st[0].stats)
     os.chdir(path_rslt5)
-    tr.write(station + '_4_10Hz', format = 'SAC')
+    tr.write(station[:-4] + '_4_10Hz.sac', format = 'SAC')
 
 
 
