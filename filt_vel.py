4a252fdd8fb9c2e9e96f3e47365165272676af45
Deleplanque Tristan <deleplanque@ipgp.fr>
2017-08-13 14:47:34 +0200

    correction ecriture path

--- filt_vel.py
+++ filt_vel.py
@@ -1,4 +1,5 @@
 from obspy import read
+import numpy as np
 from obspy import Trace
 import os
 import sys
@@ -13,7 +14,7 @@ lst_frq = ['02_05', '05_1', '1_2', '2_4', '4_10']
 lst_pth_rslt = []
 
 for freq in lst_frq:
-    lst_pth_rslt[lst_frq.index(freq)] = path + '/' + dossier + '_vel_' + freq + 'Hz'
+    lst_pth_rslt.append(path + '/' + dossier + '_vel_' + freq + 'Hz')
     if os.path.isdir(lst_pth_rslt[lst_frq.index(freq)]) == False:
     	os.makedirs(lst_pth_rslt[lst_frq.index(freq)])
 
@@ -24,32 +25,37 @@ for station in lst_fch:
 
     os.chdir(path_data)
     st = read(station)
-    tr = Trace(st[0].filter('bandpass', freqmin = 0.2, freqmax = 0.5, corners = 4, zerophase = True), st[0].stats)
-    os.chdir(path_rslt1)
+    tr = st[0].filter('bandpass', freqmin = 0.2, freqmax = 0.5, corners = 4, zerophase = True)
+    tr = Trace(np.asarray(tr), st[0].stats)
+    os.chdir(lst_pth_rslt[0])
     tr.write(station[:-4] + '_02_05Hz.sac', format = 'SAC')
     
     os.chdir(path_data)
     st = read(station)
-    tr = Trace(st[0].filter('bandpass', freqmin = 0.5, freqmax = 1, corners = 4, zerophase = True), st[0].stats)
-    os.chdir(path_rslt2)
+    tr = st[0].filter('bandpass', freqmin = 0.5, freqmax = 1, corners = 4, zerophase = True)
+    tr = Trace(np.asarray(tr), st[0].stats)
+    os.chdir(lst_pth_rslt[1])
     tr.write(station[:-4] + '_05_1Hz.sac', format = 'SAC')
 
     os.chdir(path_data)
     st = read(station)
-    tr = Trace(st[0].filter('bandpass', freqmin = 1, freqmax = 2, corners = 4, zerophase = True), st[0].stats)
-    os.chdir(path_rslt3)
+    tr = st[0].filter('bandpass', freqmin = 1, freqmax = 2, corners = 4, zerophase = True)
+    tr = Trace(np.asarray(tr), st[0].stats)
+    os.chdir(lst_pth_rslt[2])
     tr.write(station[:-4] + '_1_2Hz.sac', format = 'SAC')
 
     os.chdir(path_data)
     st = read(station)
-    tr = Trace(st[0].filter('bandpass', freqmin = 2, freqmax = 4, corners = 4, zerophase = True), st[0].stats)
-    os.chdir(path_rslt4)
+    tr = st[0].filter('bandpass', freqmin = 2, freqmax = 4, corners = 4, zerophase = True)
+    tr = Trace(np.asarray(tr), st[0].stats)
+    os.chdir(lst_pth_rslt[3])
     tr.write(station[:-4] + '_2_4Hz.sac', format = 'SAC')
 
     os.chdir(path_data)
     st = read(station)
-    tr = Trace(st[0].filter('bandpass', freqmin = 4, freqmax = 10, corners = 4, zerophase = True), st[0].stats)
-    os.chdir(path_rslt5)
+    tr = st[0].filter('bandpass', freqmin = 4, freqmax = 10, corners = 4, zerophase = True)
+    tr = Trace(np.asarray(tr), st[0].stats)
+    os.chdir(lst_pth_rslt[4])
     tr.write(station[:-4] + '_4_10Hz.sac', format = 'SAC')
 
 
