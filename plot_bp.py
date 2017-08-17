ceacfe5e4563ffce7d31787d21e30a3c0eeb9385
Deleplanque Tristan <deleplanque@ipgp.fr>
2017-08-17 16:52:39 +0200

    traite toutes les bandes de frequences

--- plot_bp.py
+++ plot_bp.py
@@ -12,20 +12,27 @@ if tp_data != '3comp' and tp_data != 'hori' and tp_data != 'vert':
 
 path_origin = os.getcwd()[:-6]
 path = path_origin + '/Kumamoto/' + dossier
-path_data = path + '/' + dossier + '_vel_2_4Hz_' + tp_data + '_env_results'
 
-os.chdir(path_data)
-with open('stack_vel_2_4Hz_' + tp_data + '_env', 'rb') as my_fch:
-    my_dpck = pickle.Unpickler(my_fch)
-    stack = my_dpck.load()
+lst_frq = ['02_05', '05_1', '1_2', '2_4', '4_10']
+lst_pth_dt = []
 
-fig, ax = plt.subplots(1, 1)
-ax.set_xlabel('Time')
-ax.set_ylabel('Strike')
-ax.imshow(stack, cmap = 'jet', interpolation = 'none', origin = 'lower')
-ax.set_aspect('auto')
+for freq in lst_frq:
+    lst_pth_dt.append(path + '/' + dossier + '_vel_' + freq + 'Hz_' + tp_data + '_env_results')
 
-fig.savefig('bp_vel_2_4Hz_' + tp_data + '_env.pdf')
+for freq in lst_frq:
+    stack = None
+    os.chdir(lst_pth_dt[lst_frq.index(freq)])
+    with open('stack_vel_' + freq + 'Hz_' + tp_data + '_env', 'rb') as my_fch:
+    	my_dpck = pickle.Unpickler(my_fch)
+    	stack = my_dpck.load()
+
+    fig, ax = plt.subplots(1, 1)
+    ax.set_xlabel('Time')
+    ax.set_ylabel('Strike')
+    ax.imshow(stack, cmap = 'jet', interpolation = 'none', origin = 'lower')
+    ax.set_aspect('auto')
+
+    fig.savefig('bp_vel_' + freq + 'Hz_' + tp_data + '_env.pdf')
 
 
 
