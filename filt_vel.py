27cccd49c96ba34eaa5e61f966d4aa343880b85e
Deleplanque Tristan <deleplanque@ipgp.fr>
2017-08-20 17:37:12 +0200

    boucle aussi sur les bandes de frequences tapering avant filtrage

--- filt_vel.py
+++ filt_vel.py
@@ -10,58 +10,68 @@ path_origin = os.getcwd()[:-6]
 path = path_origin + '/Kumamoto/' + dossier
 path_data = path + '/' + dossier + '_vel'
 
-lst_frq = ['02_05', '05_1', '1_2', '2_4', '4_10']
+lst_frq_mi = ['02', '05', '1', '2', '4']
+lst_frq_ma = ['05', '1', '2', '4', '10']
 lst_pth_rslt = []
 
-for freq in lst_frq:
-    lst_pth_rslt.append(path + '/' + dossier + '_vel_' + freq + 'Hz')
-    if os.path.isdir(lst_pth_rslt[lst_frq.index(freq)]) == False:
-    	os.makedirs(lst_pth_rslt[lst_frq.index(freq)])
+for freq in lst_frq_mi:
+    lst_pth_rslt.append(path + '/' + dossier + '_vel_' + freq + '_' + lst_frq_ma[lst_frq_mi.index(freq)] + 'Hz')
+    if os.path.isdir(lst_pth_rslt[lst_frq_mi.index(freq)]) == False:
+    	os.makedirs(lst_pth_rslt[lst_frq_mi.index(freq)])
 
-lst_fch = os.listdir(path_data)
+lst_fch_x = [a for a in os.listdir(path_data) if ('EW' in a) == True]
+lst_fch_y = [a for a in os.listdir(path_data) if ('NS' in a) == True]
+lst_fch_z = [a for a in os.listdir(path_data) if ('UD' in a) == True]
 
-for station in lst_fch:
-    print('     ', station)
+lst_fch_x.sort()
+lst_fch_y.sort()
+lst_fch_z.sort()
 
-    os.chdir(path_data)
-    st = read(station)
-    st.detrend(type = 'constant')
-    tr = st[0].filter('bandpass', freqmin = 0.2, freqmax = 0.5, corners = 4, zerophase = True)
-    tr = Trace(np.asarray(tr), st[0].stats)
-    os.chdir(lst_pth_rslt[0])
-    tr.write(station[:-4] + '_02_05Hz.sac', format = 'SAC')
-    
-    os.chdir(path_data)
-    st = read(station)
-    st.detrend(type = 'constant')
-    tr = st[0].filter('bandpass', freqmin = 0.5, freqmax = 1, corners = 4, zerophase = True)
-    tr = Trace(np.asarray(tr), st[0].stats)
-    os.chdir(lst_pth_rslt[1])
-    tr.write(station[:-4] + '_05_1Hz.sac', format = 'SAC')
+for freq in lst_frq_mi:
+    print('   ', freq, lst_frq_ma[lst_frq_mi.index(freq)])
+    if freq == '02':
+    	fqmi = 0.2
+    	fqma = 0.5
+    elif freq == '05':
+    	fqmi = 0.5
+    	fqma = 1
+    elif freq == '1':
+    	fqmi = 1
+    	fqma = 2
+    elif freq == '2':
+    	fqmi = 2
+    	fqma = 4
+    elif freq == '4':
+    	fqmi = 4
+    	fqma = 10
+    else:
+    	print('ERROR LIST_FREQ')
+    	sys.exit(0
+)
+    for station in lst_fch_x:
+    	print('     ', station)
 
-    os.chdir(path_data)
-    st = read(station)
-    st.detrend(type = 'constant')
-    tr = st[0].filter('bandpass', freqmin = 1, freqmax = 2, corners = 4, zerophase = True)
-    tr = Trace(np.asarray(tr), st[0].stats)
-    os.chdir(lst_pth_rslt[2])
-    tr.write(station[:-4] + '_1_2Hz.sac', format = 'SAC')
+    	os.chdir(path_data)
+    	stx = read(station)
+    	sty = read(lst_fch_y[lst_fch_x.index(station)])
+    	stz = read(lst_fch_z[lst_fch_x.index(station)])
+    	stx.detrend(type = 'constant')
+    	sty.detrend(type = 'constant')
+    	stz.detrend(type = 'constant')
+    	stx[0].taper(0.05, type = 'hann', max_length = None, side = 'both')
+    	sty[0].taper(0.05, type = 'hann', max_length = None, side = 'both')
+    	stz[0].taper(0.05, type = 'hann', max_length = None, side = 'both')
+    	tr_x = stx[0].filter('bandpass', freqmin = fqmi, freqmax = fqma, corners = 4, zerophase = False)
+    	tr_y = sty[0].filter('bandpass', freqmin = fqmi, freqmax = fqma, corners = 4, zerophase = False)
+    	tr_z = stz[0].filter('bandpass', freqmin = fqmi, freqmax = fqma, corners = 4, zerophase = False)
+    	tr_x = Trace(np.asarray(tr_x), stz[0].stats)
+    	tr_y = Trace(np.asarray(tr_y), stz[0].stats)
+    	tr_z = Trace(np.asarray(tr_z), stz[0].stats)
 
-    os.chdir(path_data)
-    st = read(station)
-    st.detrend(type = 'constant')
-    tr = st[0].filter('bandpass', freqmin = 2, freqmax = 4, corners = 4, zerophase = True)
-    tr = Trace(np.asarray(tr), st[0].stats)
-    os.chdir(lst_pth_rslt[3])
-    tr.write(station[:-4] + '_2_4Hz.sac', format = 'SAC')
-
-    os.chdir(path_data)
-    st = read(station)
-    st.detrend(type = 'constant')
-    tr = st[0].filter('bandpass', freqmin = 4, freqmax = 10, corners = 4, zerophase = True)
-    tr = Trace(np.asarray(tr), st[0].stats)
-    os.chdir(lst_pth_rslt[4])
-    tr.write(station[:-4] + '_4_10Hz.sac', format = 'SAC')
+    	os.chdir(lst_pth_rslt[lst_frq_mi.index(freq)])
+    	tr_x.write(station[:-4] + '_' + freq + '_' + lst_frq_ma[lst_frq_mi.index(freq)] + 'Hz.sac', format = 'SAC')
+    	tr_y.write(lst_fch_y[lst_fch_x.index(station)][:-4] + '_' + freq + '_' + lst_frq_ma[lst_frq_mi.index(freq)] + 'Hz.sac', format = 'SAC')
+    	tr_z.write(lst_fch_z[lst_fch_x.index(station)][:-4] + '_' + freq + '_' + lst_frq_ma[lst_frq_mi.index(freq)] + 'Hz.sac', format = 'SAC')
 
 
 
