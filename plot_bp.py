45aec8b02c9eb2f010a44edd9d759c41d20b645a
Deleplanque Tristan <deleplanque@ipgp.fr>
2017-08-20 17:38:25 +0200

    affiche les unites sur les axes modification des ticklabels

--- plot_bp.py
+++ plot_bp.py
@@ -27,10 +27,13 @@ for freq in lst_frq:
     	stack = my_dpck.load()
 
     fig, ax = plt.subplots(1, 1)
-    ax.set_xlabel('Time')
-    ax.set_ylabel('Strike')
+    ax.set_xlabel('Time (s)')
+    ax.set_ylabel('Strike (km)')
     ax.imshow(stack, cmap = 'jet', interpolation = 'none', origin = 'lower')
     ax.set_aspect('auto')
+    ax.set_xticklabels([0, 0, 5, 10, 15, 20, 25, 30])
+    ax.set_yticklabels([0, 0, 20, 40, 60, 80, 100])
+    print(stack.max()/len(lst_pth_dt[0]))
 
     fig.savefig('bp_vel_' + freq + 'Hz_' + tp_data + '_env.pdf')
 
