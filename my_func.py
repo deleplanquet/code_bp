b4aa9e8a8cdd77394d5f82c3845fe222b4707c2a
Deleplanque Tristan <deleplanque@ipgp.fr>
2017-08-28 16:59:53 +0200

    ajout de fonctions

--- my_func.py
+++ my_func.py
@@ -42,6 +42,34 @@ def r2d(angle):
 
 def norm(v):
     """
+    normalisation: sum(vi*vi) = 1
     """
     Norm = math.sqrt(v[0]*v[0] + v[1]*v[1] + v[2]*v[2])
-    return []
+    return [v[0]/Norm, v[1]/Norm, v[2]/Norm]
+
+def rotation(u, theta, OM):
+    """
+    rotation of a vector in 3D, return a vector
+
+    u: should have 3 components
+    theta: should be in degree
+    OM: should have 3 components
+    """
+    a = norm(OM)[0]
+    b = norm(OM)[1]
+    c = norm(OM)[2]
+    rth = d2r(theta)
+    mat = array([[a*a + (1 - a*a)*math.cos(rth),
+    	    	  a*b*(1 - math.cos(rth)) - c*math.sin(rth),
+    	    	  a*c*(1 - math.cos(rth)) + b*math.sin(rth)],
+    	    	 [a*b*(1 - math.cos(rth)) + c*math.sin(rth),
+    	    	  b*b + (1 - b*b)*math.cos(rth),
+    	    	  b*c*(1 - math.cos(rth)) - a*math.sin(rth)],
+    	    	 [a*c*(1 - math.cos(rth)) - b*math.sin(rth),
+    	    	  b*c*(1 - math.cos(rth)) + a*math.sin(rth),
+    	    	  c*c + (1 - c*c)*math.cos(rth)]])
+    v = array([[u[0]],
+    	       [u[1]],
+    	       [u[2]]])
+    v_rot = dot(mat, v)
+    return (v_rot[0][0], v_rot[1][0], v_rot[2][0])
