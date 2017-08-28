57b0b82df5d8c7dd554260327a56768c7fc74649
Deleplanque Tristan <deleplanque@ipgp.fr>
2017-08-28 12:40:11 +0200

    modification path adaptation aux bandes de frequences

--- select_stat_env.py
+++ select_stat_env.py
@@ -7,46 +7,58 @@ import numpy as np
 
 dossier = sys.argv[1]
 
-path = os.getcwd()[:-6] + '/Data/Kumamoto/' + dossier
-path_data = path + '/' + dossier + '_vel_env/'
-path_resultsP = path + '/' + dossier + '_vel_env_selectP/'
-path_resultsS = path + '/' + dossier + '_vel_env_selectS/'
+path_origin = os.getcwd()[:-6]
+path = path_origin + '/Kumamoto/' + dossier
 
-if os.path.isdir(path_resultsP) == False:
-    os.makedirs(path_resultsP)
+lst_frq = ['02_05', '05_1', '1_2', '2_4', '4_8', '8_16', '16_30']
+lst_pth_dt = []
 
-if os.path.isdir(path_resultsS) == False:
-    os.makedirs(path_resultsS)
+for freq in lst_frq:
+    lst_pth_dt.append(path + '/' + dossier + '_vel_' + freq + 'Hz_hori_env')
 
-list_station = os.listdir(path_data)
-list_station = [a for a in list_station if ('DS_Store' in a) == False]
+lst_pth_rslt_P = []
+lst_pth_rslt_S = []
 
-for station in list_station:
-    os.chdir(path_data)
-    stP = read(station)
-    stS = read(station)
-    arrival_P = stP[0].stats.starttime + stP[0].stats.sac.a
-    arrival_S = stS[0].stats.starttime + stS[0].stats.sac.t0
-    delai_PS = arrival_S - arrival_P
-    if delai_PS < 5:
-        stP[0].trim(arrival_P, arrival_P + delai_PS - 0.1)
-    else:
-        stP[0].trim(arrival_P, arrival_P + 5)
-    stS[0].trim(arrival_S, stS[0].stats.endtime)
-    trP = stP[0]
-    trS = stS[0]
-    #rapport_PS.append(math.log10(trP.max()/trS.max()))
-    rapport_PS = math.log10(trP.max()/trS.max())
-    if rapport_PS > math.log10(3):
-        st = read(station)
-        os.chdir(path_resultsP)
-        tr = Trace(st[0].data, st[0].stats)
-        tr.write(station, format='SAC')
-    if rapport_PS < math.log10(1./3):
-        st = read(station)
-        os.chdir(path_resultsS)
-        tr = Trace(st[0].data, st[0].stats)
-        tr.write(station, format='SAC')
+for freq in lst_frq:
+    lst_pth_rslt_P.append(path + '/' + dossier + '_vel_' + freq + 'Hz_hori_env_P')
+    lst_pth_rslt_S.append(path + '/' + dossier + '_vel_' + freq + 'Hz_hori_env_S')
+    if os.path.isdir(lst_pth_rslt_P[lst_frq.index(freq)]) == False:
+    	os.makedirs(lst_pth_rslt_P[lst_frq.index(freq)])
+    if os.path.isdir(lst_pth_rslt_S[lst_frq.index(freq)]) == False:
+    	os.makedirs(lst_pth_rslt_S[lst_frq.index(freq)])
+
+lst_fch = []
+
+for freq in lst_frq:
+    lst_fch.append(os.listdir(lst_pth_dt[lst_frq.index(freq)]))
+
+for freq in lst_frq:
+    print('     ', freq)
+    for station in lst_fch[lst_frq.index(freq)]:
+    	os.chdir(lst_pth_dt[lst_frq.index(freq)])
+    	stP = read(station)
+    	stS = read(station)
+    	arrival_P = stP[0].stats.starttime + stP[0].stats.sac.a
+    	arrival_S = stS[0].stats.starttime + stS[0].stats.sac.t0
+    	delai_PS = arrival_S - arrival_P
+    	if delai_PS < 5:
+    	    stP[0].trim(arrival_P, arrival_P + delai_PS + 0.1)
+    	else:
+    	    stP[0].trim(arrival_P, arrival_P + 5)
+    	stS[0].trim(arrival_S, stS[0].stats.endtime)
+    	trP = stP[0]
+    	trS = stS[0]
+    	rapport_PS = math.log10(trP.max()/trS.max())
+    	if rapport_PS > math.log10(3):
+    	    st = read(station)
+    	    os.chdir(lst_pth_rslt_P[lst_frq.index(freq)])
+    	    tr = Trace(st[0].data, st[0].stats)
+    	    tr.write(station, format = 'SAC')
+    	if rapport_PS < math.log10(1./3):
+    	    st = read(station)
+    	    os.chdir(lst_pth_rslt_S[lst_frq.index(freq)])
+    	    tr = Trace(st[0].data, st[0].stats)
+    	    tr.write(station, format = 'SAC')
 
 
 
