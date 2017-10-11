2c5982cb7fa2effea68317ddba0b871cf3a7c9b9
Tristan Deleplanque <deleplanque@ipgp.fr>
2017-10-11 12:20:43 +0900

    va chercher les parametres dans parametres_bin ne tient compte que des parametres entres, ne tourne pas sur d'autres

--- env2smooth.py
+++ env2smooth.py
@@ -4,76 +4,37 @@ from obspy import Trace
 import sys
 import os
 import numpy as np
-
-dossier = sys.argv[1]
+import pickle
 
 path_origin = os.getcwd()[:-6]
+os.chdir(path_origin + '/Kumamoto')
+with open('parametres_bin', 'rb') as my_fch:
+    my_dpck = pickle.Unpickler(my_fch)
+    param = my_dpck.load()
+
+dossier = param['dossier']
+couronne = param['couronne']
+frq = param['band_freq']
+dt_type = param['composante']
+l_smooth = param['smooth']
+
 path = path_origin + '/Kumamoto/' + dossier
+path_data = path + '/' + dossier + '_vel_' + couronne + 'km_' + frq + 'Hz/' + dossier + '_vel_' + couronne + 'km_' + frq + 'Hz_' + dt_type + '_env'
+path_results = path_data + '_smooth'
 
-lst_frq = ['02_05', '05_1', '1_2', '2_4', '4_8', '8_16', '16_30']
-lst_cpn = ['3comp', 'hori', 'vert']
+if os.path.isdir(path_results) == False:
+    os.makedirs(path_results)
 
-lst_pth_dt3 = []
-lst_pth_dth = []
-lst_pth_dtv = []
-lst_pth_rslt3 = []
-lst_pth_rslth = []
-lst_pth_rsltv = []
+lst_fch = []
 
-for freq in lst_frq:
-    pth_dt = path + '/' + dossier + '_vel_' + freq + 'Hz/' + dossier + '_vel_' + freq + 'Hz_'
-    lst_pth_dt3.append(pth_dt + '3comp_env')
-    lst_pth_dth.append(pth_dt + 'hori_env')
-    lst_pth_dtv.append(pth_dt + 'vert_env')
-    lst_pth_rslt3.append(pth_dt + '3comp_env_smooth')
-    lst_pth_rslth.append(pth_dt + 'hori_env_smooth')
-    lst_pth_rsltv.append(pth_dt + 'vert_env_smooth')
+lst_fch = os.listdir(path_data)
 
-    if os.path.isdir(lst_pth_rslt3[lst_frq.index(freq)]) == False:
-        os.makedirs(lst_pth_rslt3[lst_frq.index(freq)])
-    if os.path.isdir(lst_pth_rslth[lst_frq.index(freq)]) == False:
-        os.makedirs(lst_pth_rslth[lst_frq.index(freq)])
-    if os.path.isdir(lst_pth_rsltv[lst_frq.index(freq)]) == False:
-        os.makedirs(lst_pth_rsltv[lst_frq.index(freq)])
-
-lst_fch_3 = []
-lst_fch_h = []
-lst_fch_v = []
-
-for pth in lst_pth_dt3:
-    lst_fch_3.append(os.listdir(pth))
-for pth in lst_pth_dth:
-    lst_fch_h.append(os.listdir(pth))
-for pth in lst_pth_dtv:
-    lst_fch_v.append(os.listdir(pth))
-
-for freq in lst_frq:
-    print('     ', freq)
-    for station in lst_fch_3[lst_frq.index(freq)]:
-        os.chdir(lst_pth_dt3[lst_frq.index(freq)])
-        st = read(station)
-        tr = Trace(smooth(st[0].data, int(0.5/st[0].stats.delta)), st[0].stats)
-        os.chdir(lst_pth_rslt3[lst_frq.index(freq)])
-        tr.write(station[:-4] + '_smooth.sac', format = 'SAC')
-
-    for station in lst_fch_h[lst_frq.index(freq)]:
-        os.chdir(lst_pth_dth[lst_frq.index(freq)])
-        st = read(station)
-        tr = Trace(smooth(st[0].data, int(0.5/st[0].stats.delta)), st[0].stats)
-        os.chdir(lst_pth_rslth[lst_frq.index(freq)])
-        tr.write(station[:-4] + '_smooth.sac', format = 'SAC')
-
-    for station in lst_fch_v[lst_frq.index(freq)]:
-        os.chdir(lst_pth_dtv[lst_frq.index(freq)])
-        st = read(station)
-        tr = Trace(smooth(st[0].data, int(0.5/st[0].stats.delta)), st[0].stats)
-        os.chdir(lst_pth_rsltv[lst_frq.index(freq)])
-        tr.write(station[:-4] + '_smooth.sac', format = 'SAC')
-
-
-
-
-
+for station in lst_fch:
+    os.chdir(path_data)
+    st = read(station)
+    tr = Trace(smooth(st[0].data, int(l_smooth/st[0].stats.delta)), st[0].stats)
+    os.chdir(path_results)
+    tr.write(station[:-4] + '_smooth.sac', format = 'SAC')
 
 
 
