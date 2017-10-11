36dfac78070c228eb6a82676b80f8afbddbc2a82
Tristan Deleplanque <deleplanque@ipgp.fr>
2017-10-11 12:45:37 +0900

    va chercher les parametres dans parametres_bin

--- select_stat_env.py
+++ select_stat_env.py
@@ -4,66 +4,58 @@ import os
 import sys
 from obspy import Trace
 import numpy as np
-
-dossier = sys.argv[1]
+import pickle
 
 path_origin = os.getcwd()[:-6]
+os.chdir(path_origin + '/Kumamoto')
+with open('parametres_bin', 'rb') as my_fch:
+    my_dpck = pickle.Unpickler(my_fch)
+    param = my_dpck.load()
+
+dossier = param['dossier']
+couronne = param['couronne']
+frq = param['band_freq']
+dt_type = param['composante']
+
 path = path_origin + '/Kumamoto/' + dossier
+path_data = path + '/' + dossier + '_vel_' + couronne + 'km_' + frq + 'Hz/' + dossier + '_vel_' + couronne 'km_' + frq + 'Hz_' + dt_type + '_env_smooth'
+path_results_P = path_data + '_P'
+path_results_S = path_data + '_S'
 
-lst_frq = ['02_05', '05_1', '1_2', '2_4', '4_8', '8_16', '16_30']
-lst_pth_dt = []
-
-for freq in lst_frq:
-    lst_pth_dt.append(path + '/' + dossier + '_vel_' + freq + 'Hz/' + dossier + '_vel_' + freq + 'Hz_hori_env_smooth')
-
-lst_pth_rslt_P = []
-lst_pth_rslt_S = []
-
-for freq in lst_frq:
-    pth_dt = path + '/' + dossier + '_vel_' + freq + 'Hz/' + dossier + '_vel_' + freq + 'Hz'
-    lst_pth_rslt_P.append(pth_dt + '_hori_env_smooth_P')
-    lst_pth_rslt_S.append(pth_dt + '_hori_env_smooth_S')
-    if os.path.isdir(lst_pth_rslt_P[lst_frq.index(freq)]) == False:
-    	os.makedirs(lst_pth_rslt_P[lst_frq.index(freq)])
-    if os.path.isdir(lst_pth_rslt_S[lst_frq.index(freq)]) == False:
-    	os.makedirs(lst_pth_rslt_S[lst_frq.index(freq)])
+if os.path.isdir(path_results_P) == False:
+    os.makedirs(path_results_P)
+if os.path.isdir(path_results_S) == False:
+    os.makedirs(path_results_S)
 
 lst_fch = []
 
-for freq in lst_frq:
-    lst_fch.append(os.listdir(lst_pth_dt[lst_frq.index(freq)]))
+lst_fch = os.listdir(path_data)
 
-for freq in lst_frq:
-    print('     ', freq)
-    for station in lst_fch[lst_frq.index(freq)]:
-    	os.chdir(lst_pth_dt[lst_frq.index(freq)])
-    	stP = read(station)
-    	stS = read(station)
-    	arrival_P = stP[0].stats.starttime + stP[0].stats.sac.a
-    	arrival_S = stS[0].stats.starttime + stS[0].stats.sac.t0
-    	delai_PS = arrival_S - arrival_P
-    	if delai_PS < 5:
-    	    stP[0].trim(arrival_P, arrival_P + delai_PS + 0.1)
-    	else:
-    	    stP[0].trim(arrival_P, arrival_P + 5)
-    	stS[0].trim(arrival_S, stS[0].stats.endtime)
-    	trP = stP[0]
-    	trS = stS[0]
-    	rapport_PS = math.log10(trP.max()/trS.max())
-    	if rapport_PS > math.log10(3):
-    	    st = read(station)
-    	    os.chdir(lst_pth_rslt_P[lst_frq.index(freq)])
-    	    tr = Trace(st[0].data, st[0].stats)
-    	    tr.write(station, format = 'SAC')
-    	if rapport_PS < math.log10(1./3):
-    	    st = read(station)
-    	    os.chdir(lst_pth_rslt_S[lst_frq.index(freq)])
-    	    tr = Trace(st[0].data, st[0].stats)
-    	    tr.write(station, format = 'SAC')
-
-
-
-
+for station in lst_fch:
+    os.chdir(path_data)
+    stP = read(station)
+    stS = read(station)
+    arrival_P = stP[0].stats.starttime + stP[0].stats.sac.a
+    arrival_S = stS[0].stats.starttime + stS[0].stats.sac.t0
+    delai_PS = arrival_S - arrival_P
+    if delai_PS < 5:
+        stP[0].trim(arrival_P, arrival_P + delai_PS + 0.1)
+    else:
+        stP[0].trim(arrival_P, arrival_P + 5)
+    stS[0].trim(arrival_S, stS[0].stats.endtime)
+    trP = stP[0]
+    trS = stS[0]
+    rapport_PS = math.log10(trS.max()/trP.max())
+    if rapport_PS > math.log10(3):
+        st = read(station)
+        os.chdir(path_results_S)
+        tr = Trace(st[0].data, st[0].stats)
+        tr.write(station, format = 'SAC')
+    elif rapport_PS < math.log10(1./3):
+        st = read(station)
+        os.chdir(path_results_P)
+        tr = Trace(st[0].data, st[0].stats)
+        tr.write(station, format = 'SAC')
 
 
 
