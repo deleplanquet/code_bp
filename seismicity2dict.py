955024394340edae660365085a9133a2306f7e5a
Tristan Deleplanque <deleplanque@ipgp.fr>
2017-11-02 12:06:02 +0100

    cree un dictionnaire binaire contenant la seismicite de Kumamoto

---
+++ seismicity2dict.py
@@ -0,0 +1,27 @@
+import pickle
+import os
+
+path_origin = os.getcwd()[:-6]
+path = path_origin + '/Kumamoto'
+
+A = {}
+
+os.chdir(path)
+with open('seismicity.txt', 'r') as my_fch:
+    for line in my_fch:
+        a = {}
+        my_list = line.split()
+        a['year'] = float(my_list[0])
+        a['month'] = float(my_list[1])
+        a['day'] = float(my_list[2])
+        a['min'] = float(my_list[3])
+        a['sec'] = float(my_list[4])
+        a['lat'] = float(my_list[6])
+        a['lon'] = float(my_list[7])
+        a['dep'] = float(my_list[8])
+        a['Mj'] = float(my_list[9])
+        A[my_list[0] + my_list[1] + my_list[2] + my_list[3] + my_list[4]] = a
+
+with open('seismicity_bin', 'wb') as my_fch:
+    my_pck = pickle.Pickler(my_fch)
+    my_pck.dump(A)
