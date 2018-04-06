e473bfca54da10a486b32a544ddbef92aeefea7d
Deleplanque Tristan <deleplanque@ipgp.fr>
2018-04-06 18:42:39 +0200

    ajout carte_SoverP.py

--- routine_traitement.sh
+++ routine_traitement.sh
@@ -1,11 +1,20 @@
 #!/bin/bash
 
 python3 select_couronne.py
+
 python3 acc2vel.py
+
 python3 filt_vel.py
+
 python3 3components.py
+
 python3 vel2env.py
+
 python3 env2smooth.py
+python3 carte_SoverP.py
+
 python3 vitesse_PS.py
+
 python3 select_stat_env.py
+
 python3 select_station_angle.py
