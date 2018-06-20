6dac88a8c8b1e747cea4ee08b30e65c9b7602630
Tristan Deleplanque <deleplanque@ipgp.fr>
2018-06-20 15:26:43 +0900

    carte pour article

--- seismicity.py
+++ seismicity.py
@@ -2,6 +2,7 @@ import os
 from obspy.imaging.beachball import beach
 import pickle
 from mpl_toolkits.basemap import Basemap
+from matplotlib.patches import Polygon
 import matplotlib.pyplot as plt
 import numpy as np
 
@@ -51,32 +52,223 @@ ms_lst = ['20160414212600',
           '20160415000300',
           '20160416012500']
 
+figa, axa = plt.subplots(1, 1)
+m.drawcoastlines(linewidth = 0.1)
+m.drawmapboundary(fill_color = '0.7')
+m.fillcontinents(color = '0.9',
+                 lake_color = '0.7')
+m.drawmapscale(w_bord + 0.55,
+               s_bord + 0.15,
+               lon_eq,
+               lat_eq,
+               100,
+               zorder = 6)
+m.drawparallels(np.arange(0., 90, 1),
+                labels=[1, 0, 1, 0],
+                linewidth = 0.01)
+m.drawmeridians(np.arange(0., 180, 1),
+                labels=[0, 1, 0, 1],
+                linewidth = 0.01)
+
+
 for period in range(5):
     fig, ax = plt.subplots(1, 1)
-    m.drawcoastlines(linewidth = 0.2)
-    m.fillcontinents('yellow')
-    m.drawmapscale(w_bord + 0.55, s_bord + 0.15, lon_eq, lat_eq, 100, barstyle = 'fancy', zorder = 6)
+    m.drawcoastlines(linewidth = 0.1)
+    m.drawmapboundary(fill_color = '0.7')
+    m.fillcontinents(color = '0.95',
+                     lake_color = '0.7')
+    m.drawmapscale(w_bord + 0.55,
+                   s_bord + 0.15,
+                   lon_eq,
+                   lat_eq,
+                   100,
+                   barstyle = 'fancy',
+                   zorder = 6)
 
-    for eq in seism.keys():
+    for eq in sorted(seism.keys(), reverse = True):
         lat = seism[eq]['lat']
         lon = seism[eq]['lon']
         day = seism[eq]['day']
         hou = seism[eq]['hou']
         mnn = seism[eq]['min']
         per = None
-        if lat < n_bord and lat > s_bord and lon < e_bord and lon > w_bord:
+        if lat < n_bord and lat > s_bord and lon < e_bord and lon > w_bord and seism[eq]['Mj'] > 3:
             x_epi, y_epi = m(lon, lat)
             if day < 14 or (day == 14 and hou < 21) or (day == 14 and hou == 21 and mnn < 28):
                 per = 1
+                clra = 'red'
             elif day < 15 or (day == 15 and hou < 0) or (day == 15 and hou == 0 and mnn < 3):
                 per = 2
+                clra = 'dodgerblue'
             elif day < 16 or (day == 16 and hou < 1) or (day == 16 and hou == 1 and mnn < 25):
                 per = 3
+                clra = 'dodgerblue'
+            elif day < 21 or (day == 21 and hou < 1) or (day == 21 and hou == 1 and mnn < 25):
+                per = 4
+                clra = 'black'
             else:
                 per = 4
+                clra = 'yellow'
 
             if period == 0 or (period == 1 and per == 1) or (period == 2 and per == 2) or (period == 3 and per == 3) or (period == 4 or per == 4):
-                ax.scatter(x_epi, y_epi, 3*seism[eq]['Mj']**2, marker = 'o', edgecolors = 'black', facecolors = 'none', zorder = 2, linewidth = 0.1)
+                ax.scatter(x_epi,
+                           y_epi,
+                           3*seism[eq]['Mj']**2,
+                           marker = 'o',
+                           edgecolors = 'black',
+                           facecolors = 'none',
+                           zorder = 2,
+                           linewidth = 0.1)
+
+            if period == 0 and per >= 2 and clra != 'yellow':
+                axa.scatter(x_epi,
+                            y_epi,
+                            1*seism[eq]['Mj']**2,
+                            marker = 'o',
+                            edgecolors = clra,
+                            facecolors = 'none',
+                            zorder = 2,
+                            linewidth = 0.1)
+
+    if period == 0:
+        #Mont Aso
+        #xx, yy = m(131.09, 32.88)
+        xx, yy = m(131.09, 32.88)
+        axa.text(xx,
+                yy,
+                'Mt. Aso',
+                color = 'black',
+                zorder = 4,
+                ha = 'center',
+                va = 'center',
+                fontsize = 8)
+        xx1, yy1 = m(131.03, 32.84)
+        xx2, yy2 = m(131.15, 32.84)
+        axa.plot((xx1, xx2),
+                (yy1, yy2),
+                color = 'red',
+                zorder = 3,
+                linewidth = 0.8)
+        xx1, yy1 = m(131.13, 32.92)
+        axa.plot((xx1, xx2),
+                (yy1, yy2),
+                color = 'red',
+                zorder = 3,
+                linewidth = 0.8)
+        xx2, yy2 = m(131.11, 32.90)
+        axa.plot((xx1, xx2),
+                (yy1, yy2),
+                color = 'red',
+                zorder = 3,
+                linewidth = 0.8)
+        xx1, yy1 = m(131.09, 32.96)
+        axa.plot((xx1, xx2),
+                (yy1, yy2),
+                color = 'red',
+                zorder = 3,
+                linewidth = 0.8)
+        xx2, yy2 = m(131.07, 32.90)
+        axa.plot((xx1, xx2),
+                (yy1, yy2),
+                color = 'red',
+                zorder = 3,
+                linewidth = 0.8)
+        xx1, yy1 = m(131.05, 32.92)
+        axa.plot((xx1, xx2),
+                (yy1, yy2),
+                color = 'red',
+                zorder = 3,
+                linewidth = 0.8)
+        xx2, yy2 = m(131.03, 32.84)
+        axa.plot((xx1, xx2),
+                (yy1, yy2),
+                color = 'red',
+                zorder = 3,
+                linewidth = 0.8)
+        #ax.scatter(xx, yy, 120, marker = 'o', edgecolors = 'black', facecolors = 'orange', zorder = 3, linewidth = 0.1)
+
+        for mnsh in ms_lst:
+            xx, yy = m(ref_s[mnsh]['lon'], ref_s[mnsh]['lat'])
+            
+            if mnsh == ms_lst[2]:
+                clr = 'white'
+            else:
+                clr = '0.7'
+
+            axa.scatter(xx,
+                       yy,
+                       7*ref_s[mnsh]['Mw']**2,
+                       marker = '*',
+                       edgecolors = 'black',
+                       facecolors = clr,
+                       zorder = 4,
+                       linewidth = 0.3)
+
+        #Kumamoto city
+        xx, yy = m(131, 32.6)
+        axa.text(xx,
+                   yy,
+                   'Kumamoto',
+                   color = 'black',
+                   zorder = 4,
+                   ha = 'center',
+                   va = 'center',
+                   fontsize = 8)
+
+        xx, yy = m(130.2, 32.8)
+        axa.text(xx,
+                   yy,
+                   'Nagasaki',
+                   color = 'black',
+                   zorder = 4,
+                   ha = 'center',
+                   va = 'center',
+                   fontsize = 8)
+
+        xx, yy = m(130.1, 33.3)
+        axa.text(xx,
+                   yy,
+                   'Saga',
+                   color = 'black',
+                   zorder = 4,
+                   ha = 'center',
+                   va = 'center',
+                   fontsize = 8)
+
+        xx, yy = m(130.7, 33.5)
+        axa.text(xx,
+                   yy,
+                   'Fukuoka',
+                   color = 'black',
+                   zorder = 4,
+                   ha = 'center',
+                   va = 'center',
+                   fontsize = 8)
+
+        xx, yy = m(131.6, 33)
+        axa.text(xx,
+                   yy,
+                   'Oita',
+                   color = 'black',
+                   zorder = 4,
+                   ha = 'center',
+                   va = 'center',
+                   fontsize = 8)
+
+        xx, yy = m(131.3, 32.2)
+        axa.text(xx,
+                   yy,
+                   'Miyazaki',
+                   color = 'black',
+                   zorder = 4,
+                   ha = 'center',
+                   va = 'center',
+                   fontsize = 8)
+
+        #xx, yy = m(130.85, 32.8)
+        #ax.text(xx, yy, 'Kumamoto', color = 'red', zorder = 3, ha = 'left', va = 'center', fontsize = 20)
+
+        figa.savefig('seismicite_article.pdf')
 
     for mainsh in ms_lst:
         xx, yy = m(ref_s[mainsh]['lon'], ref_s[mainsh]['lat'])
@@ -90,8 +282,19 @@ for period in range(5):
         else:
             clr = 'blue'
 
-        ax.scatter(xx, yy, 20*ref_s[mainsh]['Mw']**2, marker = '*', color = clr, zorder = 4, linewidth = 0.1)
-        bb = beach(angles, xy = (bx, by), facecolor = clr, width = 20000, linewidth = 0.1)
+        ax.scatter(xx,
+                   yy,
+                   20*ref_s[mainsh]['Mw']**2,
+                   marker = '*',
+                   edgecolors = 'white',
+                   facecolor = clr,
+                   zorder = 4,
+                   linewidth = 0.1)
+        bb = beach(angles,
+                   xy = (bx, by),
+                   facecolor = clr,
+                   width = 20000,
+                   linewidth = 0.1)
         bb.set_zorder(4)
         ax.add_collection(bb)
 
