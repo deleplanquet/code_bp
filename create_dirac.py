d6b74144fd721249978a8caabf9feaa00a9863df
Deleplanque Tristan <deleplanque@ipgp.fr>
2019-02-11 10:30:23 +0100

    details

--- create_dirac.py
+++ create_dirac.py
@@ -93,8 +93,10 @@ for fichier in lst_fch:
             tend = tstart + 50
             tr = Trace(np.asarray(tr, np.ndarray), st[0].stats)
             tr = tr.trim(tstart, tend, pad = True, fill_value = 0)
-            tr.stats.sac.t0 = stz[0].stats.sac.a
-            tr.stats.sac.a = stz[0].stats.sac.a
+            #tr.stats.sac.t0 = stz[0].stats.sac.a
+            #tr.stats.sac.a = stz[0].stats.sac.a
+            tr.stats.sac.t0 = 5
+            tr.stats.sac.a = 5
             os.chdir(path_results)
             if dst <= 100:
                 if ('EW2' in fichier) or ('NS2' in fichier) or ('UD2' in fichier) == True:
