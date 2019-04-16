5c96db4a5dbdd29604401d646143d3842972e109
Deleplanque Tristan <deleplanque@ipgp.fr>
2019-04-16 18:48:10 +0200

    ajout de commentaires changement de noms de variables et adaptation au changement des noms de variables dans parametres.py fonctionne en apparence, a verifier

--- tosac.py
+++ tosac.py
@@ -7,38 +7,55 @@ import pickle
 
 path_origin = os.getcwd()[:-6]
 os.chdir(path_origin + '/Kumamoto')
+# load parameters given earlier by the user
 with open('parametres_bin', 'rb') as my_fch:
     my_dpck = pickle.Unpickler(my_fch)
     param = my_dpck.load()
 
-dossier = param['dossier']
+# all the parameters are not used, only the following one
+event = param['event']
 
-path = path_origin + '/Kumamoto/' + dossier
-path_data = path + '/' + dossier + '_brut'
-path_kik = path_data + '/' + dossier + '.kik'
-path_knt = path_data + '/' + dossier + '.knt'
-path_sac = path_origin + '/Kumamoto/' + dossier + '/' + dossier + '_sac'
+# path_kik and path_knt are the locations of original files (traces) for both
+# networks
+path_data = (path_origin + '/'
+             + 'Kumamoto/'
+             + event + '/'
+             + event + '_brut')
+path_kik = (path_data + '/'
+            + event + '.kik')
+path_knt = (path_data + '/'
+            + event + '.knt')
+# path_sac is the target directory after conversion of the traces to SAC
+path_sac = (path_origin + '/'
+            + 'Kumamoto/'
+            + event + '/'
+            + event + '_sac')
 
+# check the existence of the directory
 if os.path.isdir(path_sac) == False:
     os.makedirs(path_sac)
 
-list_fichiers = os.listdir(path_kik)
-list_fichiers = [a for a in list_fichiers if ('ps.gz' in a) == False]
+# make a list of files from KiK-net network
+list_files = os.listdir(path_kik)
+list_files = [a for a in list_files if 'ps.gz' not in a]
 
-for fichier in list_fichiers:
+# change format of those files into SAC format and save them in path_sac
+for f in list_files:
     os.chdir(path_kik)
-    tr = read(fichier)[0]
+    tr = read(f)[0]
     tr.stats.sac = tr.stats.knet
     os.chdir(path_sac)
-    tr.write(fichier + '.sac', format='SAC')
+    tr.write(f + '.sac', format='SAC')
 
-list_fichiers = os.listdir(path_knt)
-list_fichiers = [a for a in list_fichiers if ('ps.gz' in a) == False]
+# make a list of files from K-NET network
+list_files = os.listdir(path_knt)
+list_files = [a for a in list_files if 'ps.gz' not in a]
 
-for fichier in list_fichiers:
+# change format of those files into SAC format and save them in path_sac
+for f in list_files:
     os.chdir(path_knt)
-    tr = read(fichier)[0]
+    tr = read(f)[0]
     tr.stats.sac = tr.stats.knet
     os.chdir(path_sac)
-    tr.write(fichier + '.sac', format='SAC')
+    tr.write(f + '.sac', format='SAC')
 
