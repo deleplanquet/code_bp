6798080531f211ff6098bc55a1b08735357dd070
Deleplanque Tristan <deleplanque@ipgp.fr>
2019-04-25 13:16:22 +0200

    comments + update parameters + proper way to create directory

--- env2smooth.py
+++ env2smooth.py
@@ -1,3 +1,5 @@
+# smoothing of the envelopes
+
 from obspy import read
 from obspy.signal.util import smooth
 from obspy import Trace
@@ -6,39 +8,66 @@ import os
 import numpy as np
 import pickle
 
-print('')
-print('      python3 env2smooth.py')
+print('#################################',
+    '\n###   python3 env2smooth.py   ###',
+    '\n#################################')
 
-path_origin = os.getcwd()[:-6]
-os.chdir(path_origin + '/Kumamoto')
+# open the file of the parameters given by the user through parametres.py and
+# load them
+root_folder = os.getcwd()[:-6]
+os.chdir(root_folder + '/Kumamoto')
 with open('parametres_bin', 'rb') as my_fch:
     my_dpck = pickle.Unpickler(my_fch)
     param = my_dpck.load()
 
-dossier = param['dossier']
-couronne = param['couronne']
-frq = param['band_freq']
-dt_type = param['composante']
-l_smooth = param['smooth']
+# all the parameters are not used in this script, only the following ones
+event = param['event']
+couronne = param['hypo_interv']
+frq_bnd = param['frq_band']
+cpnt = param['component']
+l_smooth = param['l_smooth']
 
-path = path_origin + '/Kumamoto/' + dossier
-path_data = path + '/' + dossier + '_vel_' + couronne + 'km_' + frq + 'Hz/' + dossier + '_vel_' + couronne + 'km_' + frq + 'Hz_' + dt_type + '_env'
-path_results = path_data + '_smooth'
+# directories used in this script
+# - path_data is the directory of the envelopes that passed previous conditions
+# - path_rslt is the directory where the smoothed envelopes will be stored
+path_data = (root_folder + '/'
+             + 'Kumamoto/'
+             + event + '/'
+             + 'vel/'
+             + couronne + 'km_' + frq_bnd + 'Hz_' + cpnt + '/'
+             + 'env')
+path_rslt = (root_folder + '/'
+             + 'Kumamoto/'
+             + event + '/'
+             + 'vel/'
+             + couronne + 'km_' + frq_bnd + 'Hz_' + cpnt + '/'
+             + 'env_smooth')
 
-if os.path.isdir(path_results) == False:
-    os.makedirs(path_results)
+# create the directory path_rslt in case it does not exist
+if not os.path.isdir(path_rslt):
+    try:
+        os.makedirs(path_rslt)
+    except OSError:
+        print('Creation of the directory {} failed'.format(path_rslt))
+    else:
+        print('Successfully created the directory {}'.format(path_rslt))
+else:
+    print('{} is already existing'.format(path_rslt))
 
-lst_fch = []
-
+# pick the envelopes from the directory path_data
 lst_fch = os.listdir(path_data)
 
-for station in lst_fch:
+print('Smoothing of the envelopes')
+for s in lst_fch:
     os.chdir(path_data)
-    st = read(station)
-    tr = Trace(smooth(st[0].data, int(l_smooth/st[0].stats.delta)), st[0].stats)
+    # load the envelope
+    st = read(s)
+    # smooth the envelope
+    tr = smooth(st[0].data, int(l_smooth/st[0].stats.delta))
+    # preparation for SAC format
+    tr = Trace(tr, st[0].stats)
+    # save the file
     os.chdir(path_results)
     tr.write(station[:-4] + '_smooth.sac', format = 'SAC')
-
-
-
-
+    print('The envelope of the station {}'.format(s[:6]),
+            'has been successfully smoothed')
