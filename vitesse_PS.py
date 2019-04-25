8087d2d67a1bcc34a3b6d5d53880420545e4fa70
Deleplanque Tristan <deleplanque@ipgp.fr>
2019-04-25 17:21:56 +0200

    upgrade in progress

--- vitesse_PS.py
+++ vitesse_PS.py
@@ -1,3 +1,5 @@
+#
+
 from obspy import read
 import pickle
 import os
@@ -27,14 +29,11 @@ def fit_lineaire_P(x_data, b):
         y_data[i] = 5.8 * x_data[i] + b
     return y_data
 
-#constantes
-R_Earth = 6400
-
-#conversion angle degre -> radian
+# conversion angle degree -> radian
 def d2r(angle):
     return angle*math.pi/180
 
-#conversion coordonnees geographiques -> cartesien
+# conversion geographic coordinates - > cartesian coordinates
 def geo2cart(vect):
     r = vect[0]
     rlat = d2r(vect[1])
@@ -44,38 +43,43 @@ def geo2cart(vect):
     zz = r*math.sin(rlat)
     return [xx, yy, zz]
 
-#distance entre deux points, coordonnees cartesiennes
+# distance between two points whose coordinates are cartesians
 def dist(vect1, vect2):
     x1, y1, z1 = geo2cart(vect1)
     x2, y2, z2 = geo2cart(vect2)
     return pow(pow(x1 - x2, 2) + pow(y1 - y2, 2) + pow(z1 - z2, 2), 0.5)
 
-#normalisation avec max = 1
+# normalisation with max = 1
 def norm1(vect):
     norm_v = 0
     for a in vect:
         norm_v = norm_v + a*a
     return [50*a/pow(norm_v, 0.5) for a in vect]
 
-print('')
-print('      python3 vitesse_PS.py')
+print('#################################',
+    '\n###   python3 vitesse_PS.py   ###',
+    '\n#################################')
 
-path_origin = os.getcwd()[:-6]
-os.chdir(path_origin + '/Kumamoto')
+# open the file of the parameters given by the user through parametres.py and
+# load them
+root_folder = os.getcwd()[:-6]
+os.chdir(root_folder + '/Kumamoto')
 with open('parametres_bin', 'rb') as my_fch:
     my_dpck = pickle.Unpickler(my_fch)
     param = my_dpck.load()
 
+# load location of the studied earthquake
 with open('ref_seismes_bin', 'rb') as my_fch:
     my_dpck = pickle.Unpickler(my_fch)
     dict_seis = my_dpck.load()
 
-dossier = param['dossier']
-couronne = param['couronne']
-frq = param['band_freq']
-dt_type = param['composante']
-velP = param['vP']
-velS = param['vS']
+# all the parameters are not used in this script, only the following ones
+event = param['event']
+couronne = param['hypo_interv']
+frq_bnd = param['frq_band']
+cpnt = param['component']
+vP = param['vP']
+vS = param['vS']
 
 yea_seis = int(dict_seis[dossier]['nFnet'][0:4])
 mon_seis = int(dict_seis[dossier]['nFnet'][4:6])
@@ -85,23 +89,27 @@ min_seis = int(dict_seis[dossier]['nFnet'][10:12])
 sec_seis = int(dict_seis[dossier]['nFnet'][12:14])
 mse_seis = int(dict_seis[dossier]['nFnet'][14:16])
 
-t_origin_rupt = UTCDateTime(yea_seis, mon_seis, day_seis, hou_seis, min_seis, sec_seis, mse_seis)
+t_origin_rupt = UTCDateTime(yea_seis,
+                            mon_seis,
+                            day_seis,
+                            hou_seis,
+                            min_seis,
+                            sec_seis,
+                            mse_seis)
 
+# directories used in this script:
+# - path_data
+# - path_rslt
+path_data = (root_folder + '/'
+             + 'Kumamoto/'
+             + event + '/'
+             + 'vel/'
+             + couronne + 'km_' + frq_bnd + 'Hz_' + cpnt + '/'
+             + 'env_smooth')
+path_rslt = 
 path = (path_origin
         + '/Kumamoto/'
         + dossier)
-
-path_data = (path + '/'
-             + dossier
-             + '_vel_'
-             + couronne + 'km_'
-             + frq + 'Hz/'
-             + dossier
-             + '_vel_'
-             + couronne + 'km_'
-             + frq + 'Hz_'
-             + dt_type
-             + '_env_smooth')
 
 path_results = (path + '/'
                 + dossier
@@ -111,18 +119,19 @@ path_results = (path + '/'
                 + couronne + 'km_'
                 + frq + 'Hz')
 
+# pick the envelopes from the directory path_data
 list_sta = os.listdir(path_data)
 
 fig, ax = plt.subplots(1, 1)
 ax.set_xlabel('Source time (s)')
 ax.set_ylabel('Hypocenter distance (km)')
 
-vP = {}
-vS = {}
+vect_vP = {}
+vect_vS = {}
 
 os.chdir(path_data)
 
-vct_dst = np.zeros(50)
+vect_dst = np.zeros(50)
 
 for i, station in enumerate(list_sta):
     st = read(station)
