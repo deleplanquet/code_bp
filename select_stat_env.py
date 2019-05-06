be59f8da6b178b74e4549773f247efa84d13d7a7
Deleplanque Tristan <deleplanque@ipgp.fr>
2019-05-06 18:35:00 +0200

    comments + clean code

--- select_stat_env.py
+++ select_stat_env.py
@@ -1,4 +1,12 @@
-#
+# sort the stations into three sub group:
+# - stations S: stations with high energy of S-waves compared to P-waves, those
+# stations will be used for back projection with S velocity hypothesis
+# - stations P: stations with high energy of P-waves compared to S-waves, those
+# stations will be used for back projection with P velocity hypothesis
+# - other stations: stations with comparable energy of P and S-waves, those
+# stations will not be used for any back projection analysis
+# the criteria to sort the stations is the parameter ratioSP given by the user
+# through the script parametres.py
 
 import math
 from obspy import read
@@ -12,7 +20,7 @@ print('######################################',
     '\n###   python3 select_stat_env.py   ###',
     '\n######################################')
 
-# open the filde of the parameters given by the user through parametres.py and
+# open the file of the parameters given by the user through parametres.py and
 # load them
 root_folder = os.getcwd()[:-6]
 os.chdir(root_folder + '/Kumamoto')
@@ -28,15 +36,19 @@ cpnt = param['component']
 rSP = param['ratioSP']
 
 # directories used in this script
-# - path_data
-# - path_rslt
+# - path_data is the directory of the envelopes that passed previous conditions
+# - path_rslt_P is the directory of the envelopes selected for back projection
+# with P velocity hypothesis
+# - path_rslt_S is the directory of the envelopes selected for back projection
+# with S velocity hypothesis
 path_data = (root_folder + '/'
              + 'Kumamoto/'
              + event + '/'
              + 'vel/'
              + couronne + 'km_' + frq_bnd + 'Hz_' + cpnt + '/'
              + 'env_smooth')
-
+# path_rslt is created to prevent repetition, the two real directories to be
+# considered are path_rslt_P and path_rslt_S
 path_rslt = (root_folder + '/'
              + 'Kumamoto/'
              + event + '/'
@@ -46,25 +58,6 @@ path_rslt_P = (path_rslt
                + 'env_smooth_P')
 path_rslt_S = (path_rslt
                + 'env_smooth_S')
-
-#path = (path_origin
-#        + '/Kumamoto/'
-#        + dossier)
-#
-#path_data = (path + '/'
-#             + dossier
-#             + '_vel_'
-#             + couronne + 'km_'
-#             + frq + 'Hz/'
-#             + dossier
-#             + '_vel_'
-#             + couronne + 'km_'
-#             + frq + 'Hz_'
-#             + dt_type
-#             + '_env_smooth')
-#
-#path_results_P = path_data + '_P'
-#path_results_S = path_data + '_S'
 
 # create the directories path_rslt_P and path_rslt_S in case they do not exist
 if not os.path.isdir(path_rslt_P):
@@ -95,26 +88,38 @@ for s in lst_fch:
     # load the envelope twice to prevent confusion
     stP = read(s)
     stS = read(s)
-    # 
+    # define picked arrival time of both P ans S waves
     arrival_P = stP[0].stats.starttime + stP[0].stats.sac.a
     arrival_S = stS[0].stats.starttime + stS[0].stats.sac.t0
-
+    # trim differently the two traces to isolate the energies of P and S waves
     stP[0].trim(arrival_P, arrival_S - 0.1)
     stS[0].trim(arrival_S, stS[0].stats.endtime)
+    # consider the trace
     trP = stP[0]
     trS = stS[0]
     # ratio between max value of S energy and max value of P energy
-    rapport_PS = math.log10(trS.max()/trP.max())
-    #print(trS.max()/trP.max())
-    if rapport_PS > math.log10(rSP):
-        #print('   S')
+    rapport_SP = trS.max()/trP.max()
+    print('The ratio between the energy of S-waves and the energy of P-waves',
+            'of the station {}'.format(s[:6]),
+            'is equal to {:.1f}'.format(rapport_SP),
+            end = ' ')
+    # for stations with 'high' S energy
+    if rapport_SP > rSP:
         st = read(s)
         os.chdir(path_rslt_S)
         tr = Trace(st[0].data, st[0].stats)
         tr.write(s[:6], format = 'SAC')
-    if rapport_PS < math.log10(1./rSP):
-        #print('   P')
+        print('which is higher than {}'.format(rSP),
+                '\n   --> station selected for back projection of S-waves')
+    # for stations wilh 'high' P energy
+    elif rapport_SP < 1./rSP:
         st = read(s)
         os.chdir(path_rslt_P)
         tr = Trace(st[0].data, st[0].stats)
         tr.write(s[:6], format = 'SAC')
+        print('which is lower than {:.2f}'.format(1./rsP),
+                '\n   --> station selected for back projection of P-waves')
+    # for remaining stations
+    else:
+        print('which is between {:.2f} and {}'.format(1./rSP, rSP),
+                '\n   --> station NOT selected for back projection')
