cca0ac90e43702e7263555b6060a312836971a1a
Tristan Deleplanque <deleplanque@ipgp.fr>
2019-05-26 19:31:50 +0200

    preparation for the loop

--- prestack2stack.py
+++ prestack2stack.py
@@ -1,7 +1,67 @@
 #
 
 import os
+import pickle
 
 print('#####################################',
     '\n###   python3 prestack2stack.py   ###',
     '\n#####################################')
+
+# open the file of the parameters given by the user through parametres.py and
+# load them
+root_folder = os.getcwd()[:-6]
+os.chdir(root_folder + '/Kumamoto')
+with open('parametres_bin', 'rb') as mfch:
+    mdpk = pickle.Unpickler(mfch)
+    param = mdpk.load()
+
+# all the parameters are not used in this script, only the following ones
+event = param['event']
+frq_bnd = param['frq_band']
+cpnt = param['component']
+couronne = param['hypo_interv']
+hyp_bp = param['selected_waves']
+angle = param['angle']
+
+# directories used in this script
+#
+#
+path_data = (root_folder + '/'
+             + 'Kumamoto/'
+             + event + '/'
+             + 'results/'
+             + 'vel_env_' + frq_bnd + 'Hz_' + cpnt + '_smooth/'
+             + 'others')
+path_sta = (root_folder + '/'
+            + 'Kumamoto/'
+            + event + '/'
+            + 'vel_env_selection/'
+            + frq_bnd + 'Hz_' + cpnt + '_smooth_' + couronne + 'km_'
+                + hyp_bp + '_' + angle + 'deg')
+path_rslt = (root_folder + '/'
+             + 'Kumamoto/'
+             + event + '/'
+             + 'results/'
+             + 'vel_env_' + frq_bnd + 'Hz_' + cpnt + '_smooth/'
+             + couronne + 'km_' + hyp_bp + '_' + angle + 'deg/'
+             + 'others')
+
+# create path_rslt directory in case it does not exist
+if not os.path.isdir(path_rslt):
+    try:
+        os.makedirs(path_rslt)
+    except OSError:
+        print('Creation of the directory {} failed'.format(path_rslt))
+    else:
+        print('Successfully created the directory {}'.format(path_rslt))
+else:
+    print('{} is already existing'.format(path_rslt))
+
+# load the 4D back projection cube
+os.chdir(path_data)
+with open(event + '_vel_env_' + frq_bnd + 'Hz_' + cpnt
+          + '_smooth_' + hyp_bp + '_prestack',
+          'rb') as mfch:
+    mdpk = pickle.Unpickler(mfch)
+    prestack = mdpk.load()
+
