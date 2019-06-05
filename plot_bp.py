a25e70632d64f468da42aef3daf80d59b9f29ced
Tristan Deleplanque <deleplanque@ipgp.fr>
2019-06-05 18:46:25 +0200

    detail name stack

--- plot_bp.py
+++ plot_bp.py
@@ -84,7 +84,7 @@ os.chdir(path_data)
 stack = None
 with open(event + '_vel_env_' + frq_bnd + 'Hz_' + cpnt + '_smooth_'
           + couronne + 'km_' + hyp_bp + '_' + azim + 'deg_'
-          + 'stack', 'rb') as mfch:
+          + 'it-0_stack', 'rb') as mfch:
     mdpk = pickle.Unpickler(mfch)
     stack = mdpk.load()
 
