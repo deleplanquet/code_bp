ac3ee49ee4a1f5de0f4a538afd63bcc4e60594a1
Deleplanque Tristan <deleplanque@ipgp.fr>
2019-06-12 13:12:04 +0200

    second figure to show the position of the maximum of the stack among iterations

--- plot_maxstck_fct_it.py
+++ plot_maxstck_fct_it.py
@@ -2,6 +2,8 @@
 
 import matplotlib.pyplot as plt
 import pickle
+import os
+import numpy as np
 
 print('##########################################',
     '\n###   python3 plot_maxstck_fct_it.py   ###',
@@ -22,10 +24,20 @@ cpnt = param['component']
 couronne = param['hypo_interv']
 hyp_bp = param['selected_waves']
 azim = param['angle']
+l_grid = param['l_grid']
+w_grid = param['w_grid']
+l_grid_step = param['l_grid_step']
+w_grid_step = param['w_grid_step']
 
 # directories used in this script
 #
 #
+path_nb_it = (root_folder + '/'
+              + 'Kumamoto/'
+              + event + '/'
+              + 'results/'
+              + 'vel_env_' + frq_bnd + 'Hz_' + cpnt + '_smooth/'
+              + couronne + 'km_' + hyp_bp + '_' + azim + 'deg')
 path_data = (root_folder + '/'
              + 'Kumamoto/'
              + event + '/'
@@ -38,7 +50,7 @@ path_rslt = (root_folder + '/'
              + event + '/'
              + 'results/'
              + 'vel_env_' + frq_bnd + 'Hz_' + cpnt + '_smooth/'
-             + couronne = 'km_' + hyp_bp + '_' + azim + 'deg/'
+             + couronne + 'km_' + hyp_bp + '_' + azim + 'deg/'
              + 'miscellaneous_plots')
 
 # create the directory path_rslt in case it does not exist
@@ -48,14 +60,34 @@ if not os.path.isdir(path_rslt):
     except OSError:
         print('Creation of the directory {} failed'.format(path_rslt))
     else:
-        print('Successfully created the directory {}'.format(path-rslt))
+        print('Successfully created the directory {}'.format(path_rslt))
 else:
     print('{} is already existing'.format(path_rslt))
 
-lst_it = os.listdir(path_data)
-lst_it = [a for a in lst_it if '_stack' in a]
+# load the original back projection stack to get the maximum
+os.chdir(path_data)
+with open(event + '_vel_env_' + frq_bnd + 'Hz_' + cpnt + '_smooth_'
+            + couronne + 'km_' + hyp_bp + '_' + azim + 'deg_'
+            + 'it-0_stack', 'rb') as mfch:
+    mdpk = pickle.Unpickler(mfch)
+    stck_orgn = mdpk.load()
 
-fig, ax = plt.subplots(1, 1)
+stck_or_mx = stck_orgn[:, :, :].max()
+
+lst_it = os.listdir(path_nb_it)
+lst_it = [a for a in lst_it if 'iteration-' in a]
+
+fig1, ax1 = plt.subplots(1, 1)
+ax1.set_xlabel('Number of iterations')
+ax1.set_ylabel('Global maximum of the stack\n'
+                + '(normalized by iteration 0 value)')
+ax1.set_xlim([0, 1.1*len(lst_it)])
+ax1.set_ylim([0, 1.1])
+fig2, ax2 = plt.subplots(1, 1)
+ax2.set_xlabel('Along strike (km)')
+ax2.set_ylabel('Down dip (km)')
+ax2.set_xlim([-l_grid*l_grid_step/2, l_grid*l_grid_step/2])
+ax2.set_ylim([-w_grid*w_grid_step/2, w_grid*w_grid_step/2])
 os.chdir(path_data)
 for it in lst_it:
     with open(event + '_vel_env_' + frq_bnd + 'Hz_' + cpnt + '_smooth_'
@@ -64,9 +96,20 @@ for it in lst_it:
         mdpk = pickle.Unpickler(mfch)
         stck = mdpk.load()
     stck_mx = stck[:, :, :].max()
-    ax.scatter(int(it[10:]), stck_mx)
+    ax1.scatter(int(it[10:]), stck_mx/stck_or_mx, s = 10, color = 'black')
+    # red star with white border for the hypocenter
+    ax2.scatter(0, 0, 250, marker = '*', color = 'white', lw = 0.2, zorder = 0)
+    ax2.scatter(0, 0, 150, marker = '*', color = 'red', lw = 0.2, zorder = 1)
+    ax2.scatter(np.where(stck == stck_mx)[1] - l_grid/2 + int(it[10:])/10,
+                np.where(stck == stck_mx)[2] - w_grid/2,
+                s = 3,
+                color = 'black',
+                zorder = 2)
 
 os.chdir(path_rslt)
-fig.savefig(event + '_vel_env_' + frq_bnd + 'Hz_' + cpnt + '_smooth_'
+fig1.savefig(event + '_vel_env_' + frq_bnd + 'Hz_' + cpnt + '_smooth_'
             + couronne + 'km_' + hyp_bp + '_' + azim + 'deg_'
             + 'max_fct_it.pdf')
+fig2.savefig(event + '_vel_env_' + frq_bnd + 'Hz_' + cpnt + '_smooth_'
+            + couronne + 'km_' + hyp_bp + '_' + azim + 'deg_'
+            + 'posmax_fct_it.pdf')
