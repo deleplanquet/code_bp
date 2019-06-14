62cf2c18e87aaf1b2fe9549666917450a4d333e0
Deleplanque Tristan <deleplanque@ipgp.fr>
2019-06-14 14:03:11 +0200

    comments

--- prestack2stack.py
+++ prestack2stack.py
@@ -1,4 +1,5 @@
-#
+# build 3D stack from 4D prestack by summing among the dimension of stations,
+# only the stations on the selected file will be considered for the summation
 
 import os
 import pickle
@@ -31,8 +32,10 @@ bp_len_t = param['bp_length_time']
 bp_samp_rate = param['bp_samp_rate']
 
 # directories used in this script
-#
-#
+# - path_data is the directory of the 4D prestack
+# - path_sta is the directory where there is a list of the stations to consider
+#   for the stack
+# - path_rslt is the directory where the stack will be stored
 path_data = (root_folder + '/'
              + 'Kumamoto/'
              + event + '/'
@@ -72,15 +75,18 @@ with open(event + '_vel_env_' + frq_bnd + 'Hz_' + cpnt
     mdpk = pickle.Unpickler(mfch)
     prestack = mdpk.load()
 
+# pick the list of stations that we want to consider for the stack since the
+# stack is not always the sum among all the stations
 lst_sta = os.listdir(path_sta)
 lst_sta = [a for a in lst_sta if '.sac' in a]
+# initialization of the stack
 stack = np.zeros((int(bp_len_t*bp_samp_rate),
                   int(l_grid/l_grid_step),
                   int(w_grid/w_grid_step)))
-
+# building the stack
 for s in lst_sta:
     stack = stack + prestack[s[:6]]
-
+#save the stack
 os.chdir(path_rslt)
 with open(event + '_vel_env_' + frq_bnd + 'Hz_'
           + cpnt + '_smooth_' + couronne + 'km_'
